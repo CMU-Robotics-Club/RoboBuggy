e7fdfafd0e0a3776d46ed1bf26d32f61b0a9e8e9
ayeohmy <ayeohmy@gmail.com>
2014-07-22 22:07:12 -0700

    #2 Created the bones of the IO layer

---
+++ code/IO_types.h
@@ -0,0 +1,26 @@
+/********************************************************************************
+ * All rights are open to the public, to use, share, dump, spread for the       *
+ * foreseeable future. This software and documentation constitutes purely 		*
+ * published and public work, and contains open source knowledge by a bunch		*
+ * of college kids who just want to have fun. All the material and code may be 	*
+ * used, copied, duplicated, changed, disclosed... with any human's free will.	*
+ * Have a nice day! :)															*
+ ********************************************************************************/
+
+#ifndef IO_TYPES_H_
+#define IO_TYPES_H_
+
+typedef enum 
+{  
+	LOW,
+	HIGH
+}IO_state_e;
+
+
+typedef enum 
+{  
+	RET_OK,
+	RET_ERROR
+}IO_ret_e;
+
+#endif
