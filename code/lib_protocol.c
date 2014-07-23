b0c58e4953681bdd5ff899f7308951a5f4cc9cd3
ayeohmy <ayeohmy@gmail.com>
2014-07-23 04:56:09 +0100

    Starting to create the protocol framework #1

---
+++ code/lib_protocol.c
@@ -0,0 +1,66 @@
+/********************************************************************************
+ * All rights are open to the public, to use, share, dump, spread for the       *
+ * foreseeable future. This software and documentation constitutes purely 		*
+ * published and public work, and contains open source knowledge by a bunch		*
+ * of college kids who just want to have fun. All the material and code may be 	*
+ * used, copied, duplicated, changed, disclosed... with any human's free will.	*
+ * Have a nice day! :)															*
+ ********************************************************************************/
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "lib_types.h"
+
+
+/********************************************************************************
+*                     			 D E F I N I T I O N S
+********************************************************************************/
+#define HEAD 0xFC
+#define ONE_BYTE_SIZE 8
+#define TWO_BYTE_SIZE 16
+
+
+
+/********************************************************************************
+*                      L O C A L    P R O T O T Y P E S
+********************************************************************************/
+static uint32_t create_msg( uint8_t id, uint16_t msg);
+static Ret_E send_msg( uint32_t message );
+
+/********************************************************************************
+*                       G L O B A L    F U N C T I O N S
+********************************************************************************/
+/* This will set up the head, and append all the messages ready to be sent and 
+ * prepare the message
+ * And then send the message with a return state being ok if successful 
+ */
+Ret_E protocol_send( uint8_t id, uint16_t msg )
+{
+	Ret_E success = RET_ERROR;
+	// create the message
+	uint32_t message = create_msg(id, msg);
+	// if more messages, append? << What do?
+
+	// send the message
+	send_msg( message );
+	// return state
+	return success;
+
+}
+
+
+/********************************************************************************
+*                       L O C A L    F U N C T I O N S
+********************************************************************************/
+
+static uint32_t create_msg( uint8_t id, uint16_t msg)
+{
+	uint8_t message = HEAD;
+	message = message << ONE_BYTE_SIZE;
+	message = message | id;
+	message = message << TWO_BYTE_SIZE;
+	message = message | msg;
+	return message;
+}
---
+++ code/lib_types.h
@@ -0,0 +1,25 @@
+/********************************************************************************
+ * All rights are open to the public, to use, share, dump, spread for the       *
+ * foreseeable future. This software and documentation constitutes purely 		*
+ * published and public work, and contains open source knowledge by a bunch		*
+ * of college kids who just want to have fun. All the material and code may be 	*
+ * used, copied, duplicated, changed, disclosed... with any human's free will.	*
+ * Have a nice day! :)															*
+ ********************************************************************************/
+
+#ifndef LIB_TYPES_H
+#define LIB_TYPES_H
+
+#include <stdint.h>
+
+
+typedef enum 
+{
+	RET_OK,
+	RET_ERROR,
+	RET_BUFFER_UNAVAILABLE,
+	RET_NOT_INITIALIZED
+}Ret_E;
+
+
+#endif // LIB_TYPES_H
