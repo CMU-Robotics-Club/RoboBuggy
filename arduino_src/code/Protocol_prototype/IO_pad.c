3fcd4470a418fd2a425d4362935329e9fa9b34aa
Audrey 'Yo <ayeohmy@gmail.com>
2014-09-07 01:11:27 -0400

    closes #1 closes #2 This commit adds the basic protocol for send and receive in the code. The file is in arduino_src > code > Protocol Prototype. Send and receive uses 1 Serial line. The receive looks for a header 0xFC. Code still needs to be cleaned up Further testing is still needed.

---
+++ arduino_src/code/Protocol_prototype/IO_pad.c
@@ -0,0 +1,48 @@
+/********************************************************************************
+ * All rights are open to the public, to use, share, dump, spread for the       *
+ * foreseeable future. This software and documentation constitutes purely 		*
+ * published and public work, and contains open source knowledge by a bunch		*
+ * of college kids who just want to have fun. All the material and code may be 	*
+ * used, copied, duplicated, changed, disclosed... with any human's free will.	*
+ * Have a nice day! :)															*
+ ********************************************************************************/
+
+#include <Arduino.h>
+#include "IO_pad.h"
+#include "lib_types.h"
+/*********************************************************************************
+ *						L O C A L    P R O T O T Y P E S						 *
+ ********************************************************************************/
+
+
+
+
+
+/*********************************************************************************
+ *						G L O B A L    F U N C T I O N S						 *
+ ********************************************************************************/
+Ret_E IO_set_pad( byte pad, byte value )
+{
+	Ret_E success = RET_ERROR;
+	analogWrite(pad, value);
+	success = RET_OK;
+	return success;
+}
+
+
+Ret_E IO_set_state( byte pad, IO_state_e state)
+{
+	Ret_E success = RET_ERROR;
+	digitalWrite(pad, state);
+	success = RET_OK;
+	return success;
+}
+
+
+/*********************************************************************************
+ *							L O C A L    F U N C T I O N S						 *
+ ********************************************************************************/
+
+
+
+
