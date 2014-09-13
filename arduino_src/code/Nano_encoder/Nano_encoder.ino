4ccd23c605a6dc282dbe7c7116497e8a42ca4ffe
Audrey 'Yo <ayeohmy@gmail.com>
2014-09-13 14:15:37 -0400

    B
    Implemented the protocol for encoder. #closes 34

--- arduino_src/code/Nano_encoder/Nano_encoder.ino
+++ arduino_src/code/Nano_encoder/Nano_encoder.ino
@@ -43,15 +43,15 @@ void loop()
   unsigned int upper_message = reset_count >> 16;
   unsigned int lower_message = reset_count & 0xFF;
   // send the two parts of reset_count
-  protocol_send(ENC_BYTE_ONE_TICK_RESET, upper_message);
-  protocol_send(ENC_BYTE_TWO_TICK_RESET , lower_message);
+  Serial.println(protocol_send(ENC_BYTE_ONE_TICK_RESET, upper_message), HEX);
+  Serial.println(protocol_send(ENC_BYTE_TWO_TICK_RESET, lower_message), HEX);
   
   // split the last_count
   upper_message = last_count >> 16;
   lower_message = last_count & 0xFF;
   // send the two parts of last_count
-  protocol_send(ENC_BYTE_ONE_TICK_LAST, upper_message);
-  protocol_send(ENC_BYTE_TW0_TICK_LAST, lower_message);
+  Serial.println(protocol_send(ENC_BYTE_ONE_TICK_LAST, upper_message), HEX);
+  Serial.println(protocol_send(ENC_BYTE_TW0_TICK_LAST, lower_message), HEX);
   
   // reset the last count after the send
   last_count = 0;
@@ -64,4 +64,12 @@ void loop()
     last_looptime = millis() - loop_starttime;
   }
   /************* END OF TAIL OF TIMING LOOP *********/
+}
+
+void send_time(unsigned long time)
+{
+   unsigned int upper_message = time >> 16;
+   unsigned int lower_message = time & 0xFF;
+   Serial.println(protocol_send(ENC_TIMESTAMP_ONE, upper_message), HEX);
+   Serial.println(protocol_send(ENC_TIMESTAMP_TWO, lower_message), HEX);
 }
--- arduino_src/code/Protocol_prototype/Protocol_prototype.ino
+++ arduino_src/code/Protocol_prototype/Protocol_prototype.ino
@@ -22,6 +22,7 @@ void loop() {
   byte index = 0x01;
   unsigned int message = 0xDEAD;
   unsigned long packet = protocol_send(index, message);
+  Serial.println(packet, HEX);
   
   // TODO: should have a safe fault eg. packet != NULL
 //  Serial.print("The packet is: ");
@@ -30,29 +31,29 @@ void loop() {
   
   /************** RECEIVE PACKET ********************/
    // if Serial is available read if there are any incoming messages
-   if (Serial.available()) {
-          // read the incoming byte:
-          byte incomingByte = Serial.read();
-
-          // say what you got:
-//          Serial.print("I received: ");
-//          Serial.println(incomingByte, HEX);
-          
-          if(incomingByte == HEAD)
-          {
-            unsigned long pckt = incomingByte;
-            for(int i = 0; i < 3; i++)
-            {
-              pckt = pckt << ONE_BYTE_SIZE;
-              incomingByte = Serial.read();
-              pckt = pckt | incomingByte;  
-            }
-//           Serial.print("I received a packet! It is: ");
-//           Serial.println(pckt, HEX); 
-           Serial.write(pckt);
-          }
-          // parse the byte?
-          
-  } 
+//   if (Serial.available()) {
+//          // read the incoming byte:
+//          byte incomingByte = Serial.read();
+//
+//          // say what you got:
+////          Serial.print("I received: ");
+////          Serial.println(incomingByte, HEX);
+//          
+//          if(incomingByte == HEAD)
+//          {
+//            unsigned long pckt = incomingByte;
+//            for(int i = 0; i < 3; i++)
+//            {
+//              pckt = pckt << ONE_BYTE_SIZE;
+//              incomingByte = Serial.read();
+//              pckt = pckt | incomingByte;  
+//            }
+////           Serial.print("I received a packet! It is: ");
+////           Serial.println(pckt, HEX); 
+//           Serial.write(pckt);
+//          }
+//          // parse the byte?
+//          
+//  } 
   delay(500);
 }
