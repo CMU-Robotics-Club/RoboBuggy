b1339a204b22e70a3e431559b849ee0960853b55
kbrennan <kbrennan@andrew.cmu.edu>
2014-09-20 01:07:57 -0400

    Added serial reading

--- surface_src/java_src/Data Logging/src/main/Gui.java
+++ surface_src/java_src/Data Logging/src/main/Gui.java
@@ -1,27 +1,26 @@
 package main;
+
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class Gui extends JFrame {
 	private static final long serialVersionUID = 670947948979376738L;
 	
-	private static final int CAMERA_ID = 0;
+	private final static int CAMERA_ID = 0;
 	
 	private static JFrame window;
-	private static JPanel cameraPanel;
-	private static JPanel gpsPanel;
-	private static JPanel imuPanel;
+	private static CameraPanel cameraPanel;
+	private static GpsPanel gpsPanel;
 	
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 		window = new JFrame();
-		window.setVisible(true);
 		
 		// Initialize Panels for Window
-		cameraPanel = new CameraPanel(CAMERA_ID);
+		cameraPanel = new CameraPanel( CAMERA_ID );
 		gpsPanel = new GpsPanel();
-	   	
+		
 		window.add(cameraPanel);
-		//window.add(gpsPanel);
-		//window.add(imuPanel);
+		window.add(gpsPanel);
+		
+		window.setVisible(true);
 	}
 }
