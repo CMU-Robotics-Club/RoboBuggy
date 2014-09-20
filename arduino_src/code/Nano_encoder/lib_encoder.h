1cc79c8f0931caf8b74065a8155346549d6b473d
arathorn593 <arathorn593@users.noreply.github.com>
2014-09-19 21:46:48 -0500

    added new encoder library under arduino_src/code/Nano_encoder

---
+++ arduino_src/code/Nano_encoder/lib_encoder.h
@@ -0,0 +1,21 @@
+/**
+ * @file lib_encoder.h
+ * @author Joseph Paetz (rpaetz)
+ */
+#ifndef _LIB_ENCODER_H_
+#define _LIB_ENCODER_H_
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+	//sets up the encoder with the given Pin
+	void encoder_init(int encPin);
+	
+	//returns the count of the encoder
+	void get_enc_count();
+
+#ifdef __cplusplus
+} // extern "C"
+#endif /* __cplusplus */
+
+#endif /* _ENCODER_H_ */
