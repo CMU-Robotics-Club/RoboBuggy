a6b3e65155fe4f0cda05a4163dc817ca51e32b30
conwill708 <connor.willison@gmail.com>
2014-10-10 19:16:16 +0000

    Changed PWM period in receiver.c to 45000us.

--- arduino_src/RadioBuggyMega/receiver.c
+++ arduino_src/RadioBuggyMega/receiver.c
@@ -37,7 +37,7 @@
 //High pulse lasts under 1/21 of period
 
 //For 2.4 GHz racecar reciever
-#define PWM_TIME 18370
+#define PWM_TIME 45000   //the pwn period (us)
 #define PWM_THRESH 130
 #define BIG_PULSE 8
 //High pulse lasts over 1/8 of period
