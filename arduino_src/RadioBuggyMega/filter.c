6bcdde2de1448b509054d48045607682bd44d6e0
Flailey <haleydalzell@gmail.com>
2014-10-10 18:01:24 -0400

    fixing the comments, checking correctness

--- arduino_src/RadioBuggyMega/filter.c
+++ arduino_src/RadioBuggyMega/filter.c
@@ -1,3 +1,11 @@
+/**
+ * @file filter.h
+ * @author Zach Dawson (zsd)
+ *
+ * This is an averageing filter. Computes the average of 
+ * the last FILTER_LEN inputs.
+ */
+
 #include <Arduino.h>
 #include "receiver.h"
 #include "filter.h"
@@ -8,64 +16,31 @@ void filter_init(struct filter_state *state) {
     state->started = 0;
 }
 
-//<<<<<<< Updated upstream
 int filter_loop(struct filter_state *state, int val){
      int i;
     // First initialization: on assumption val is never -1
     if(!(state->started)){
+    // On Initialization, assume our "first" reading
+    //    occurred N times, and is our history
         for(i = 0; i < FILTER_LEN; i++){
             state->prev_values[i] = val;
-//=======
-//#define SIZE 5 // the array size
-//static char started = 0;
-//static int last_val_sent;
-//static int values[SIZE];
-//static int pos;
-//
-///**
-// * @brief Filters noise from raw RC readings
-// *
-// */
-//int filter(int new_raw_reading){
-//     int i;
-//   
-//    // On Initialization, assume our "first" reading
-//    //    occurred N times, and is our history
-//    if(!started){
-//        for(i = 0; i < SIZE; i++){
-//            values[i] = new_raw_reading;
-//>>>>>>> Stashed changes
         }
         state->pos = 0; // initialize the positions of the next number to be changed
         state->started = 1;
         return val;
     } 
     
-    
-//<<<<<<< Updated upstream
     // NOT first initialization. 
     state->prev_values[state->pos] = val;  // replace the number in the array
     state->pos = (state->pos+1)%FILTER_LEN;// increment the pos number
     // calculate average of array
-//=======
-//    
-//        
-//    values[pos] = val;  // replace the number in the array
-//    pos = (pos+1)%SIZE; // increment the pos number
-//    
-//    // calculate average of previous readings
-//>>>>>>> Stashed changes
     int sum = 0;
     for(i = 0; i < FILTER_LEN; i++){
         sum += state->prev_values[i];
     }
-//<<<<<<< Updated upstream
+    
+    // Return average of filter
     int avg = sum/FILTER_LEN;
-    // return.
     return avg;
-//=======
-//    // Return the average.
-//    return sum / SIZE;
-//>>>>>>> Stashed changes
 
 }
