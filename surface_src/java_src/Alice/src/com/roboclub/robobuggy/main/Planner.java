3542cb9633c84380727c088c5d144fcaa0a9af44
Trevor Decker <Mr.Trevor.decker@gmail.com>
2014-10-18 06:19:53 -0400

    added test for servo

--- surface_src/java_src/Alice/src/com/roboclub/robobuggy/main/Planner.java
+++ surface_src/java_src/Alice/src/com/roboclub/robobuggy/main/Planner.java
@@ -28,23 +28,6 @@ public class Planner implements Runnable {
 	 */
 	@Override
 	public void run() {
-		//currently a test of the servo, todo change this to a seprate test class 
-		int i = 0;
-		boolean up = true;
-		while(running) {
-			//Robot.WriteAngle(i);
-			if (up) i+=5;
-			else i-=5;
-			if (i >= 90) up = false;
-			else if (i <= 0) up = true;
-			
-			try {
-				Thread.sleep(100);
-			} catch (Exception e) {
-				Thread.currentThread().interrupt();
-			}
-		}
-		
-		Robot.ShutDown();
+		//TODO
 	}
 }
