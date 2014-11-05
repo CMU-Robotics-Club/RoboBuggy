53e8853cbf95d696556212546ea44058a5f876cc
Ian Hartwig <spacepenguine@gmail.com>
2014-11-04 22:59:02 -0500

    finish implementing reading rbsm console

--- offline_tools/miscellaneous/rbsm_console.py
+++ offline_tools/miscellaneous/rbsm_console.py
@@ -1,8 +1,13 @@
 import atexit
 import curses
-import curses.textpad
+import rbsm_lib
+import sys
+import threading
 import time
 
+# kill the main thread on sigint
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 mid_to_str = {
   0: "ENC_TICKS_LAST",
@@ -16,15 +21,18 @@ mid_to_str = {
 }
 
 
-def redraw(screen, message_cache, command_line):
+def redraw(state):
+  screen = state["screen"]
+  message_cache = state["message_cache"]
+  command_line = state["command_line"]
+  
   (max_y, max_x) = screen.getmaxyx()
   row_id = 2
   for mid in message_cache.keys():
-    staleness = message_cache[mid]["update_time"] - time.time()
     screen.addstr(row_id, 2, "                                                                 ")
     screen.addstr(row_id, 2, "%s %s %s" % (mid_to_str[mid],
                                            message_cache[mid]["data"],
-                                           staleness) )
+                                           message_cache[mid]["update_time"]) )
     row_id = row_id + 1
 
   screen.addstr(max_y-2, 2, "                                                                 ")
@@ -33,32 +41,76 @@ def redraw(screen, message_cache, command_line):
   screen.refresh()
 
 
-def main():
-  message_cache = {}
-  command_line = "asdfasdf"
-  screen = curses.initscr()
-  # screen.nodelay(1)
-  curses.noecho()
-  screen.clear()
-  screen.border(0)
-  screen.addstr(1, 2, "Message Data Staleness(s)")
+def rbsm_worker(state):
+  message_cache = state["message_cache"]
+  rbsm_endpoint = state["rbsm_endpoint"]
+
+  while(1):
+    new_message = rbsm_endpoint.read()
+    if(new_message["status"] == "locked"):
+      # save new data
+      message_cache.update([
+        ( new_message["id"], 
+          {"data": new_message["data"], "update_time": time.time()} ),
+      ])
+      # update remotely
+      redraw(state)
+
+  return None
+
+
+def command_worker(state):
+  screen = state["screen"]
 
   while(1):
     new_char = screen.getch()
     if(new_char == 8 or new_char == 127):
-      command_line = command_line[0:-1]
+      state["command_line"] = state["command_line"][0:-1]
     elif(new_char == 10 or new_char == 13):
-      command_line = ""
+      state["command_line"] = ""
     elif(new_char < 128):
-      command_line = command_line + chr(new_char)
-    # command_line = command_line + str(new_char)
+      state["command_line"] = state["command_line"] + chr(new_char)
 
-    message_cache.update([
-      (10, {"data": 2304, "update_time": time.time()}),
-      (11, {"data": 2304, "update_time": time.time()})
-    ])
+    redraw(state)
 
-    redraw(screen, message_cache, command_line)
+  return None
+
+
+
+def main():
+  state = {
+    "message_cache": {},
+    "command_line": "",
+    "screen": None,
+    "rbsm_endpoint": None,
+  }
+  screen = state["screen"]
+
+  # setup program window
+  screen = curses.initscr()
+  curses.noecho()
+  screen.clear()
+  screen.border(0)
+  screen.addstr(1, 2, "Message Data Time")
+  state["screen"] = screen
+  redraw(state)
+
+  # setup incomming messages
+  rbsm_endpoint = rbsm_lib.RBSerialMessage(sys.argv[1])
+  state["rbsm_endpoint"] = rbsm_endpoint
+  rbsm_thread = threading.Thread(target=rbsm_worker, args=(state,))
+  rbsm_thread.daemon = True
+  rbsm_thread.start()
+
+  # setup reading commands
+  command_thread = threading.Thread(target=command_worker, args=(state,))
+  command_thread.daemon = True
+  command_thread.start()
+
+  rbsm_thread.join()
+  command_thread.join()
+
+  clean_up()
 
 
 @atexit.register
