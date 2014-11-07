cf6da0731abc2c0a04739e95cbeb68c20db314e5
Ian Hartwig <spacepenguine@gmail.com>
2014-11-07 02:44:24 -0500

    rename member variables for consistency

--- arduino_src/RadioBuggyMega/pinreceiver.cpp
+++ arduino_src/RadioBuggyMega/pinreceiver.cpp
@@ -5,32 +5,16 @@ PinReceiver::PinReceiver(){
 
 }
 
-void PinReceiver::Begin(int pin, int int_num, void (*int_wrapper)() ){
-    receiver_pin_ = pin;
-    switch(receiver_pin_){
-        case 2:
-            leftmost = 2000;
-            rightmost = 980;
-            centermost = 1480;
-        case 3:
-            leftmost = -1;
-            rightmost = -1;
-            centermost = -1;
-        default:
-            leftmost = 0;
-            rightmost = 0;
-            centermost = 0;
-    }
-    pwm_time = 18370;
-    pwm_thresh = 1300;
-    big_pulse = 8;
-    short_pulse = 19;
-    // signal_time = 0;
-    // signal_diff_time = 0;
-    rc_available = 0;
-    up_switch_time = 0;
-    down_switch_time = 0;
-    rc_value = 0;
+void PinReceiver::Begin(int pin_num, int int_num, void (*int_wrapper)() ){
+    receiver_pin_ = pin_num;
+    k_pwm_time_ = 18370;
+    k_pwm_thresh_ = 1300;
+    k_big_pulse_ = 8;
+    k_short_pulse_ = 19;
+    rc_available_ = 0;
+    up_switch_time_ = 0;
+    down_switch_time_ = 0;
+    rc_value_ = 0;
 
     attachInterrupt(int_num, int_wrapper, CHANGE);
 }
@@ -38,21 +22,21 @@ void PinReceiver::Begin(int pin, int int_num, void (*int_wrapper)() ){
 
 void PinReceiver::OnInterruptReceiver(){
     if (digitalRead(receiver_pin_) == HIGH){
-        if ((micros() - up_switch_time > pwm_time - pwm_thresh) &&
-            (micros() - up_switch_time < pwm_time + pwm_thresh)) {
-            if ((down_switch_time > up_switch_time) && (rc_available == 0)) {
-                rc_value = down_switch_time - up_switch_time;
-                rc_available = 1;
-                if (rc_value / (pwm_time / big_pulse) >= 1 ||
-                    rc_value / (pwm_time / short_pulse) == 0) {
-                    rc_available == 0;
+        if ((micros() - up_switch_time_ > k_pwm_time_ - k_pwm_thresh_) &&
+            (micros() - up_switch_time_ < k_pwm_time_ + k_pwm_thresh_)) {
+            if ((down_switch_time_ > up_switch_time_) && (rc_available_ == 0)) {
+                rc_value_ = down_switch_time_ - up_switch_time_;
+                rc_available_ = 1;
+                if (rc_value_ / (k_pwm_time_ / k_big_pulse_) >= 1 ||
+                    rc_value_ / (k_pwm_time_ / k_short_pulse_) == 0) {
+                    rc_available_ == 0;
                 }
             }
         }
-        up_switch_time = micros();
+        up_switch_time_ = micros();
     } else {
-        if (up_switch_time) {
-            down_switch_time = micros();
+        if (up_switch_time_) {
+            down_switch_time_ = micros();
 
         }
     }
@@ -60,24 +44,24 @@ void PinReceiver::OnInterruptReceiver(){
 
 
 bool PinReceiver::Available() {
-    return rc_available;
+    return rc_available_;
 }
 
 
 int PinReceiver::GetAngle(){
-    int ret_val = (int)(rc_value - 980)*3/17; //WHYYYYY
-    rc_available = 0;
+    int ret_val = (int)(rc_value_ - 980)*3/17; //WHYYYYY
+    rc_available_ = 0;
     return ret_val;
 }
 
 
 void PinReceiver::PrintDebugInfo(){
     Serial1.print("up_switch_time: ");
-    Serial1.println(up_switch_time);
+    Serial1.println(up_switch_time_);
     Serial1.print("down_switch_time: ");
-    Serial1.println(down_switch_time);
+    Serial1.println(down_switch_time_);
     Serial1.print("rc_available: ");
-    Serial1.println(rc_available);
+    Serial1.println(rc_available_);
     Serial1.print("rc_value: ");
-    Serial1.println(rc_value);
+    Serial1.println(rc_value_);
 }
--- arduino_src/RadioBuggyMega/pinreceiver.h
+++ arduino_src/RadioBuggyMega/pinreceiver.h
@@ -3,16 +3,18 @@
 
 class PinReceiver {
     unsigned int receiver_pin_;
-    int leftmost, rightmost, centermost;
-    int pwm_time, pwm_thresh, big_pulse, short_pulse;
-    volatile unsigned int rc_available;
-    volatile unsigned long up_switch_time;
-    volatile unsigned long down_switch_time;
-    volatile unsigned long rc_value;
+    int k_pwm_time_;
+    int k_pwm_thresh_;
+    int k_big_pulse_;
+    int k_short_pulse_;
+    volatile unsigned int rc_available_;
+    volatile unsigned long up_switch_time_;
+    volatile unsigned long down_switch_time_;
+    volatile unsigned long rc_value_;
 
     public:
         PinReceiver();
-        void Begin(int pin, int int_num, void (*int_wrapper)() );
+        void Begin(int pin_num, int int_num, void (*int_wrapper)() );
         void OnInterruptReceiver();
         bool Available();
         int GetAngle();
