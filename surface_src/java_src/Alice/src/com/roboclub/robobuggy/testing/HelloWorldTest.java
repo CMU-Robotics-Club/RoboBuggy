a8a30f7ad0a05bc98dadbf8e9e5f81d6208fddbe
TrevorDecker <tdecker@andrew.cmu.edu>
2014-11-19 01:50:50 -0500

    Testing travis test system should fail test

---
+++ surface_src/java_src/Alice/src/com/roboclub/robobuggy/testing/HelloWorldTest.java
@@ -0,0 +1,15 @@
+public class HelloWorldTest extends junit.framework.TestCase {
+
+	public void testNothing() {
+	}
+
+	public void testWillAlwaysFail() {
+		fail("An error message");
+	}
+
+	public void testAnotherFail(){
+		System.out.println("this should fail \n");
+		fail("another error");
+	}
+
+}
