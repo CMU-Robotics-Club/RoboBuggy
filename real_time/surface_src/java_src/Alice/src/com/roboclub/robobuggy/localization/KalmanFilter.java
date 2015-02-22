cbdb12c41e222f80ed1b81ed6317c2fb4801344b
TrevorDecker <tdecker@andrew.cmu.edu>
2015-02-22 04:01:07 -0500

    Added kf protoypes

--- real_time/surface_src/java_src/Alice/src/com/roboclub/robobuggy/localization/KalmanFilter.java
+++ real_time/surface_src/java_src/Alice/src/com/roboclub/robobuggy/localization/KalmanFilter.java
@@ -2,14 +2,30 @@ package com.roboclub.robobuggy.localization;
 
 public class KalmanFilter {
 
-	public KalmanFilter() {
-		// This is a very trusting kalman filter; it just adds the
-		// encoder offset to the current believed position.
+    public KalmanFilter() {
+        // This is a very trusting kalman filter; it just adds the
+        // encoder offset to the current believed position.
+        
+    }
+    
+    public void ApplyTransitionModel(){
+        //TODO
+    }
+    
+    public void ApplyObservationModel(){
+        //TDOO
+    }
+    
+    public void reset() {
+        // TODO
+    }
 
-	}
+    public void getState(){
+        //TODO
+    }
 
-	public void reset() {
-		// TODO
-	}
-
+    public void getConfidence(){
+        //TODO
+    }
+    
 }
