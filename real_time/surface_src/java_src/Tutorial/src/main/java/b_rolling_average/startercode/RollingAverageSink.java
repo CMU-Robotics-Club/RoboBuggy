0e6395435f0dac77abaa5351f2759c7d722ec8d8
Matt Sebek <sebek.matt@gmail.com>
2015-09-24 20:41:32 -0700

    Adds second sample; b_rolling average.

---
+++ real_time/surface_src/java_src/Tutorial/src/main/java/b_rolling_average/startercode/RollingAverageSink.java
@@ -0,0 +1,28 @@
+package b_rolling_average.startercode;
+
+import com.roboclub.robobuggy.ros.Message;
+import com.roboclub.robobuggy.ros.MessageListener;
+import com.roboclub.robobuggy.ros.Node;
+import com.roboclub.robobuggy.ros.Subscriber;
+
+public class RollingAverageSink implements Node {
+
+	Subscriber s = new Subscriber("rollingAverageValues", new MessageListener() {
+		@Override
+		public void actionPerformed(String topicName, Message m) {
+			// Cast m to the correct message type. If this cast fails, an exception is thrown.
+			IntegerMessage im = (IntegerMessage) m;
+			System.out.println("Hello, Rolling Average! (" + im.val + ")");
+		}
+	});
+	
+	@Override
+	public boolean shutdown() {
+		// no resources to clean up; simply return.
+		return true;
+	}
+	
+	
+	
+}
+
---
+++ real_time/surface_src/java_src/Tutorial/src/main/java/b_rolling_average/startercode/RollingAverager.java
@@ -0,0 +1,39 @@
+package b_rolling_average.startercode;
+
+import com.roboclub.robobuggy.ros.Message;
+import com.roboclub.robobuggy.ros.MessageListener;
+import com.roboclub.robobuggy.ros.Node;
+import com.roboclub.robobuggy.ros.Publisher;
+import com.roboclub.robobuggy.ros.Subscriber;
+
+public class RollingAverager implements Node {
+
+	Publisher p;
+	Subscriber s;
+
+	// Note that if 'NumRollingElems' is 5, then the average of the last 5 
+	//		elements should be output.
+	public RollingAverager(int NumRollingElems) {
+		// Create a subscriber that listens on the channel of NumberSource.java
+		//  Every time a message comes in, get this integer, and add it to the sum.
+		// Publish this sum on to the channel RollingAverageSink is listening to.
+	
+		// If you notice any limitations/ambiguous points, make a design
+		//	decision and document it. Better decisions lead to a better
+		//  autonomous buggy.
+	}
+
+	
+	@Override
+	public boolean shutdown() {
+		// No resources-needing-closing required; shutdown.
+		//   (but if you opened a file or something, close it cleanly)
+		return true;
+	}
+	
+	
+	
+	
+	
+}
+
