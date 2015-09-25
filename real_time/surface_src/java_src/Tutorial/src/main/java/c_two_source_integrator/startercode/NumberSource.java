d05a3e4daa94f107fff74c8f4a10a47541f89380
Matt Sebek <sebek.matt@gmail.com>
2015-09-25 10:54:20 -0700

    Adding Third Sample project

---
+++ real_time/surface_src/java_src/Tutorial/src/main/java/c_two_source_integrator/startercode/NumberSource.java
@@ -0,0 +1,25 @@
+package c_two_source_integrator.startercode;
+
+import com.roboclub.robobuggy.ros.Node;
+import com.roboclub.robobuggy.ros.Publisher;
+
+public class NumberSource implements Node {
+	
+	Publisher p;
+
+	public NumberSource() {
+		p = new Publisher("values"); 
+	
+		// TODO you: finish this file; too!
+	}
+	
+	@Override
+	public boolean shutdown() {
+		return true;
+	}
+	
+	
+	
+	
+}
+
