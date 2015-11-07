cb2631a916bb011fc57e4903c0af9bff0be3427c
Vivaan Bahl <vivaanbahl@paloaltoapps.com>
2015-11-06 20:24:07 -0500

    incremental commit to json log playback
    currently overhauling the fauxrunner to take advantage of the new JSON log format

--- real_time/surface_src/java_src/Alice/src/main/java/com/roboclub/robobuggy/simulation/FauxRunner.java
+++ real_time/surface_src/java_src/Alice/src/main/java/com/roboclub/robobuggy/simulation/FauxRunner.java
@@ -1,17 +1,25 @@
 package com.roboclub.robobuggy.simulation;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-
 import java.util.Date;
 
-import com.roboclub.robobuggy.fauxNodes.FauxNode;
+import com.orsoncharts.util.json.JSONArray;
+import com.orsoncharts.util.json.JSONObject;
+import com.orsoncharts.util.json.parser.JSONParser;
+import com.orsoncharts.util.json.parser.ParseException;
 import com.roboclub.robobuggy.fauxNodes.FauxEncoderNode;
 import com.roboclub.robobuggy.fauxNodes.FauxGPSNode;
 import com.roboclub.robobuggy.fauxNodes.FauxIMUNode;
+import com.roboclub.robobuggy.fauxNodes.FauxNode;
 import com.roboclub.robobuggy.fauxNodes.FauxSteeringNode;
-import com.roboclub.robobuggy.ros.SensorChannel;
+import com.roboclub.robobuggy.main.MessageLevel;
+import com.roboclub.robobuggy.main.RobobuggyLogicException;
+import com.roboclub.robobuggy.messages.ImuMeasurement;
+import com.roboclub.robobuggy.ros.Publisher;
 
 public class FauxRunner implements Runnable {
 
@@ -21,8 +29,16 @@ public class FauxRunner implements Runnable {
 	private FauxSteeringNode drive_ctrl = null;
 	private String path;
 	
+	private Publisher imuPub;
+	private Publisher gpsPub;
+	private Publisher encoderPub;
+	private Publisher brakePub;
+	private Publisher steeringPub;
+	private Publisher loggingButtonPub;
+	
 	//Can probably improve the constructor to take all collections, but meh.
 	public FauxRunner(ArrayList<FauxNode> sensors, String path) {
+	
 		
 		//really, really ghetto. Sorry.
 		this.path = path;
@@ -60,6 +76,82 @@ public class FauxRunner implements Runnable {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Starting to read from file!");
+		try {
+			JSONParser parser = new JSONParser();
+			FileReader reader = new FileReader(path);
+			JSONObject completeLogFile = (JSONObject) parser.parse(reader); //may take a while, currently working on a solution to read line by line
+			
+			//get info from the header file
+			Date loggingDate = (Date) completeLogFile.get("date_recorded");
+			
+			long prevTimeInMillis = loggingDate.getTime();
+			
+			JSONArray sensorDataArray = (JSONArray) completeLogFile.get("sensor_data");
+			for(Object senObj : sensorDataArray) {
+				JSONObject sensor = (JSONObject)senObj;
+				Date sensorTimestamp = (Date) sensor.get("timestamp");
+				long currentSensorTimeInMillis = sensorTimestamp.getTime();
+				
+				Thread.sleep(currentSensorTimeInMillis - prevTimeInMillis);
+				
+				JSONObject sensorData = (JSONObject) sensor.get("data");
+				String sensorName = (String) sensorData.get("name");
+				
+				JSONObject sensorParams = (JSONObject) sensorData.get("params");
+				
+				switch(sensorName) {
+				
+					case "IMU":
+						float yaw = (float) sensorParams.get("yaw");
+						float pitch = (float) sensorParams.get("pitch");
+						float roll = (float) sensorParams.get("roll");
+						
+						imuPub.publish(new ImuMeasurement(yaw, pitch, roll));
+						
+						break;
+						
+					case "GPS":
+						
+						break;
+						
+					case "logging button":
+						
+						break;
+						
+					case "Steering":
+						
+						break;
+						
+					case "Encoder":
+						
+						break;
+						
+					case "Brake":
+						
+						break;
+						
+					default:
+						new RobobuggyLogicException("Found an unsupported sensor in the logs!", MessageLevel.WARNING);
+						break;
+				
+				
+				}
+				
+				
+			}
+			
+		}
+		catch(IOException e) {
+			new RobobuggyLogicException("Error while trying to read from the playback file!", MessageLevel.EXCEPTION);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block\
+			new RobobuggyLogicException("Something went wrong while waiting...", MessageLevel.WARNING);
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			new RobobuggyLogicException("Couldn't parse the file given. Make sure it's a proper JSON file", MessageLevel.EXCEPTION);
+		}
+		
 
 		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
