e598065c780994887158d94d46657a786c21f847
Vivaan Bahl <vivaanbahl@paloaltoapps.com>
2016-01-29 23:28:03 -0500

    Started the move towards a LoggingNode

--- real_time/surface_src/java_src/Alice/src/main/java/com/roboclub/robobuggy/nodes/sensors/LoggingNode.java
+++ real_time/surface_src/java_src/Alice/src/main/java/com/roboclub/robobuggy/nodes/sensors/LoggingNode.java
@@ -1,16 +1,21 @@
 package com.roboclub.robobuggy.nodes.sensors;
 
-import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+
+import com.google.gson.JsonObject;
 import com.orsoncharts.util.json.JSONObject;
+import com.roboclub.robobuggy.main.RobobuggyConfigFile;
+import com.roboclub.robobuggy.main.RobobuggyLogicNotification;
+import com.roboclub.robobuggy.main.RobobuggyMessageLevel;
 import com.roboclub.robobuggy.messages.GuiLoggingButtonMessage;
 import com.roboclub.robobuggy.nodes.baseNodes.BuggyBaseNode;
 import com.roboclub.robobuggy.nodes.baseNodes.BuggyDecoratorNode;
 import com.roboclub.robobuggy.nodes.baseNodes.SerialNode;
-import com.roboclub.robobuggy.ros.Message;
-import com.roboclub.robobuggy.ros.MessageListener;
-import com.roboclub.robobuggy.ros.Publisher;
-import com.roboclub.robobuggy.ros.NodeChannel;
-import com.roboclub.robobuggy.ros.Subscriber;
+import com.roboclub.robobuggy.ros.*;
 import com.roboclub.robobuggy.ui.Gui;
 
 /**
@@ -21,17 +26,87 @@ import com.roboclub.robobuggy.ui.Gui;
 public class LoggingNode extends BuggyDecoratorNode {
 
 	private Publisher loggingButtonPub;
-	// Get the folder that we're going to use
+	private File outputFile;
+    private NodeChannel[] filters;
+    private LinkedList<Message> messageQueue;
+    private LogWriterRunnable loggingThread;
+    private boolean keepLogging;
 
-	// TODO get folder name from file.
 	/**
 	 * Create a new {@link LoggingNode} decorator
 	 * @param channel the {@link NodeChannel} of the {@link LoggingNode}
+     * @param outputDirPath The path to the output directory (not file)
+     * @param filters sensors to log. To log all sensors, just use NodeChannel.values()
 	 */
-	public LoggingNode(NodeChannel channel) {
+	public LoggingNode(NodeChannel channel, String outputDirPath, NodeChannel...filters) {
 		super(new BuggyBaseNode(channel));
-	}
-	
+
+        this.filters = filters;
+        messageQueue = new LinkedList<>();
+        keepLogging = true;
+        loggingThread = new LogWriterRunnable();
+
+        createNewLogFile(outputDirPath);
+        setupSubscriberList();
+        startLogging();
+
+    }
+
+
+
+    /**
+     * Starts the logging process
+     */
+    private void startLogging() {
+        loggingThread.start();
+    }
+
+    /**
+     * Sets up the subscriber list - Simply enumerates over our NodeChannel filters and adds
+     * a subscriber for each one
+     */
+    private void setupSubscriberList() {
+        for (NodeChannel filter : filters) {
+            new Subscriber(filter.getMsgPath(), new MessageListener() {
+                @Override
+                public void actionPerformed(String topicName, Message m) {
+                    messageQueue.add(m);
+                }
+            });
+        }
+    }
+
+
+    /**
+     * Creates the log file, and returns the status
+     * Returns false if anything went wrong, but already throws the logic exception
+     *
+     * @param outputDirString the directory string of the log file
+     * @return the status of the operation - true if it succeeded, false if it didn't
+     */
+    private boolean createNewLogFile(String outputDirString) {
+        File outputDirectory = new File(outputDirString);
+        if (!outputDirectory.exists() || !outputDirectory.isDirectory()) {
+            new RobobuggyLogicNotification("Output directory path isn't a folder!", RobobuggyMessageLevel.EXCEPTION);
+            return false;
+        }
+
+        outputFile = new File(outputDirectory.getPath() + "/" + RobobuggyConfigFile.LOG_FILE_NAME);
+        try {
+            if(!outputFile.createNewFile()) {
+                new RobobuggyLogicNotification("Couldn't create log file!", RobobuggyMessageLevel.EXCEPTION);
+                return false;
+            }
+        } catch (IOException e) {
+            new RobobuggyLogicNotification("Error reading the filesystem!", RobobuggyMessageLevel.EXCEPTION);
+            return false;
+        }
+
+        //everything succeeded!
+        return true;
+    }
+
+
 	/**{@inheritDoc}*/
 	@Override
 	protected boolean startDecoratorNode() {
@@ -51,6 +126,53 @@ public class LoggingNode extends BuggyDecoratorNode {
 	protected boolean shutdownDecoratorNode() {
 		return true;
 	}
+
+
+    /**
+     * LogWriterRunnable - where we actually process each message and write it to the file
+     */
+    private class LogWriterRunnable extends Thread {
+
+        private int imuHits = 0;
+        private int encoderHits = 0;
+        private int gpsHits = 0;
+        private int brakeHits = 0;
+        private int fingerprintHits = 0;
+        private int logButtonHits = 0;
+        private int steeringHits = 0;
+        private int logicNotificationHits = 0;
+
+        private String name = "\"name\": \"Robobuggy Data Logs\",";
+        private String schemaVersion = "\"schema_version\": 1.0,";
+        private String dateRecorded = "\"date_recorded\": \"" +
+                new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()) + "\",";
+        private String swVersion = "\"software_version\": \"" + getCurrentSoftwareVersion() + "\",";
+        private String sensorDataHeader = "\"sensor_data\": [";
+
+        @Override
+        public synchronized void start() {
+
+            stream.println("{" + "\n    " + name + "\n    " + schemaVersion + "\n    " + dateRecorded
+                    + "\n    " + swVersion + "\n    " + sensorDataHeader);
+
+            //always want to log :)
+            while (keepLogging) {
+                //spin in a loop until a message comes in
+                while (messageQueue.isEmpty()) {}
+                //now we have a message from the queue
+            }
+        }
+
+
+        public void writeToFile(String str) {
+
+        }
+
+        public String getCurrentSoftwareVersion() {
+            return RobobuggyConfigFile.ALICE_LIBRARY_VERSION;
+        }
+    }
+
 
 	/**
 	 * Called to translate a peeled message to a JSON object
