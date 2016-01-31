adfcca15a8670a8494060557a39e1b9073aef129
Vivaan Bahl <vivaanbahl@paloaltoapps.com>
2016-01-31 17:06:55 -0500

    Changed the placement of the buttons so that it isn't random, and we get them in a list

--- real_time/surface_src/java_src/Alice/src/main/java/com/roboclub/robobuggy/ui/NodeViewer.java
+++ real_time/surface_src/java_src/Alice/src/main/java/com/roboclub/robobuggy/ui/NodeViewer.java
@@ -1,13 +1,12 @@
 package com.roboclub.robobuggy.ui;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import javax.swing.JButton;
 
 import com.roboclub.robobuggy.main.Robot;
 import com.roboclub.robobuggy.ros.Node;
-import com.roboclub.robobuggy.ros.NodeChannel;
 
 /**
  * This class creates a gui element for showing the status of every node that is currently running on buggy
@@ -16,15 +15,29 @@ import com.roboclub.robobuggy.ros.NodeChannel;
  * Note that currently the list is not updated and it only shows the connections at startup
  */
 public class NodeViewer extends RobobuggyGUIContainer{
-	
-	NodeViewer(){
-		Random ran = new Random();
+
+	// use this for organizing them in a list
+	private ArrayList<Double> nodeLocList;
+	private static final double LEFT_PADDING = .01;
+	private static final double HEIGHT_PADDING = .1;
+
+	/**
+	 * Constructor for the NodeViewer - gets all the current nodes and adds them to the screen
+	 */
+	public NodeViewer(){
+		nodeLocList = new ArrayList<>();
+		nodeLocList.add(-0.1);
+
 		Robot aRobot = Robot.getInstance();
 		List<Node> nodeList = aRobot.getNodeList();
-		for(int i = 0;i<nodeList.size();i++){
-			JButton thisNode = new JButton(nodeList.get(i).getName());
-			this.addComponent(thisNode, ran.nextDouble()*.9, ran.nextDouble()*.9, .1, .1);
-		
+
+		for (Node aNodeList : nodeList) {
+			JButton thisNode = new JButton(aNodeList.getName());
+			double buttonYpos = nodeLocList.get(nodeLocList.size() - 1) + HEIGHT_PADDING;
+			nodeLocList.add(buttonYpos);
+
+			this.addComponent(thisNode, LEFT_PADDING, buttonYpos, .1, .1);
+
 		}
 		
 		/*
