8ad9fa29e51e32aa80ecd118329bfe0fc2743961
Vivaan Bahl <vivaanbahl@paloaltoapps.com>
2016-02-22 23:44:00 -0500

    cleaned up some more - converted tabs to spaces

--- real_time/surface_src/java_src/Alice/src/main/java/com/roboclub/robobuggy/nodes/localizers/HighTrustGPSLocalizer.java
+++ real_time/surface_src/java_src/Alice/src/main/java/com/roboclub/robobuggy/nodes/localizers/HighTrustGPSLocalizer.java
@@ -24,73 +24,72 @@ import com.roboclub.robobuggy.ros.Subscriber;
  *
  */
 public class HighTrustGPSLocalizer implements Node{
-	private double buggyFrameGpsX;
-	private double buggyFrameGpsY;
-	private double buggyFrameRotZ;
+    private double buggyFrameGpsX;
+    private double buggyFrameGpsY;
+    private double buggyFrameRotZ;
 
 
-	private Publisher posePub;
+    private Publisher posePub;
 
-	/**
-	 * Constructor for the High Trust Localizer which will initialize the system to an identity (zero position) 
-	 */
-	public HighTrustGPSLocalizer(){
-		//init values
-		buggyFrameGpsX = 0.0;
-		buggyFrameGpsY = 0.0;
+    /**
+     * Constructor for the High Trust Localizer which will initialize the system to an identity (zero position)
+     */
+    public HighTrustGPSLocalizer(){
+        //init values
+        buggyFrameGpsX = 0.0;
+        buggyFrameGpsY = 0.0;
 
 
-		//Initialize subscriber to GPS measurements
-		new Subscriber(NodeChannel.GPS.getMsgPath(), new MessageListener() {
-			@Override
-			public void actionPerformed(String topicName, Message m) {
-				GpsMeasurement newGPSData = (GpsMeasurement)m;
+        //Initialize subscriber to GPS measurements
+        new Subscriber(NodeChannel.GPS.getMsgPath(), new MessageListener() {
+            @Override
+            public void actionPerformed(String topicName, Message m) {
+                GpsMeasurement newGPSData = (GpsMeasurement)m;
 
-				// Get the delta latitude and longitude, use that to figure out how far we've travelled
-				double oldGPSX = buggyFrameGpsX;
-				double oldGPSY = buggyFrameGpsY;
-				buggyFrameGpsY = newGPSData.getLongitude();
-				buggyFrameGpsX = newGPSData.getLatitude();
-				double dy = buggyFrameGpsY - oldGPSY;
-				double dx = buggyFrameGpsX - oldGPSX;
+                // Get the delta latitude and longitude, use that to figure out how far we've travelled
+                double oldGPSX = buggyFrameGpsX;
+                double oldGPSY = buggyFrameGpsY;
+                buggyFrameGpsY = newGPSData.getLongitude();
+                buggyFrameGpsX = newGPSData.getLatitude();
+                double dy = buggyFrameGpsY - oldGPSY;
+                double dx = buggyFrameGpsX - oldGPSX;
 
-				// take the arctangent in order to get the heading (in degrees)
-				buggyFrameRotZ = Math.toDegrees(Math.atan2(dy,dx));
+                // take the arctangent in order to get the heading (in degrees)
+                buggyFrameRotZ = Math.toDegrees(Math.atan2(dy,dx));
 
-				publishUpdate();
-			}
-		});
+                publishUpdate();
+            }
+        });
 
-	}
+    }
 
-	private void publishUpdate(){
-		posePub.publish(new GPSPoseMessage(new Date(), buggyFrameGpsX, buggyFrameGpsY, buggyFrameRotZ));
-	}
+    private void publishUpdate(){
+        posePub.publish(new GPSPoseMessage(new Date(), buggyFrameGpsX, buggyFrameGpsY, buggyFrameRotZ));
+    }
 
-	@Override
-	public boolean startNode() {
-		// TODO Auto-generated method stub
-		posePub = new Publisher(NodeChannel.POSE.getMsgPath());;
-		return true;
-	}
+    @Override
+    public boolean startNode() {
+        posePub = new Publisher(NodeChannel.POSE.getMsgPath());
+        return true;
+    }
 
-	@Override
-	public boolean shutdown() {
-		posePub = null;
-		buggyFrameGpsX = 0.0;
-		buggyFrameGpsY = 0.0;
-		return true;
-	}
+    @Override
+    public boolean shutdown() {
+        posePub = null;
+        buggyFrameGpsX = 0.0;
+        buggyFrameGpsY = 0.0;
+        return true;
+    }
 
-	@Override
-	public void setName(String newName) {
+    @Override
+    public void setName(String newName) {
 
-	}
+    }
 
-	@Override
-	public String getName() {
-		return "High Trust GPS Localizer";
-	}
+    @Override
+    public String getName() {
+        return "High Trust GPS Localizer";
+    }
 
 
 
