8c99eaece1132cd268b910135f252fec7fa21fbf
Vasu Agrawal <vasu_agrawal@yahoo.com>
2016-02-25 03:50:49 -0500

    Oscar's going to hate me, I keep throwing in globals to his nice JS. I'll fix it eventually.

--- real_time/surface_src/Web GUI/new/ws.js
+++ real_time/surface_src/Web GUI/new/ws.js
@@ -13,6 +13,29 @@ var started=false;
 var imageSize=0;
 var image64="";
 
+var chart_roll = new SmoothieChart({millisPerPixel:57,maxValueScale:1.5,scaleSmoothing:1,grid:{fillStyle:'rgba(0,0,0,0.39)',sharpLines:true,verticalSections:7,borderVisible:false},labels:{fontSize:20}}),
+    canvas_roll = document.getElementById('imu-chart-roll'),
+    series_roll = new TimeSeries();
+
+chart_roll.addTimeSeries(series_roll, {lineWidth:2.5,strokeStyle:'#00ffd5',fillStyle:'rgba(255,0,0,0.30)'});
+chart_roll.streamTo(canvas_roll, 0);
+
+
+var chart_pitch = new SmoothieChart({millisPerPixel:57,maxValueScale:1.5,scaleSmoothing:1,grid:{fillStyle:'rgba(0,0,0,0.39)',sharpLines:true,verticalSections:7,borderVisible:false},labels:{fontSize:20}}),
+    canvas_pitch = document.getElementById('imu-chart-pitch'),
+    series_pitch = new TimeSeries();
+
+chart_pitch.addTimeSeries(series_pitch, {lineWidth:2.5,strokeStyle:'#00ffd5',fillStyle:'rgba(255,0,0,0.30)'});
+chart_pitch.streamTo(canvas_pitch, 0);
+
+
+var chart_yaw = new SmoothieChart({millisPerPixel:57,maxValueScale:1.5,scaleSmoothing:1,grid:{fillStyle:'rgba(0,0,0,0.39)',sharpLines:true,verticalSections:7,borderVisible:false},labels:{fontSize:20}}),
+    canvas_yaw = document.getElementById('imu-chart-yaw'),
+    series_yaw = new TimeSeries();
+
+chart_yaw.addTimeSeries(series_yaw, {lineWidth:2.5,strokeStyle:'#00ffd5',fillStyle:'rgba(255,0,0,0.30)'});
+chart_yaw.streamTo(canvas_yaw, 0);
+
 function read() {
     var dataSocket = new WebSocket("ws://127.0.0.1:8080");
     dataSocket.onmessage = onDataMessage;
@@ -33,10 +56,16 @@ function updateIMU(msg) {
     var p = deg2rad(msg.pitch);
     var y = deg2rad(msg.yaw);
 
+    series_roll.append(new Date().getTime(), msg.roll);
+    series_pitch.append(new Date().getTime(), msg.pitch);
+    series_yaw.append(new Date().getTime(), msg.yaw);
+
     imu1.setAngles(r, p, y);
     imu2.setAngles(r, p, y);
     imu3.setAngles(r, p, y);
     imu4.setAngles(r, p, y);
+
+
 }
 
 function updateEncoder(msg) {
