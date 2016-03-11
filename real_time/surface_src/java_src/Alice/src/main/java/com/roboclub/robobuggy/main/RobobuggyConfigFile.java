9b240c5bb1a104c1aa5935697ea63e85ed58951d
Sean Buckley <sbuckley12489@gmail.com>
2016-03-10 23:54:29 -0500

    Forgot to add the vision sensor

--- real_time/surface_src/java_src/Alice/src/main/java/com/roboclub/robobuggy/main/RobobuggyConfigFile.java
+++ real_time/surface_src/java_src/Alice/src/main/java/com/roboclub/robobuggy/main/RobobuggyConfigFile.java
@@ -34,7 +34,7 @@ public final class  RobobuggyConfigFile {
 	public static final boolean IMU_ENABLED = true;
 	public static final boolean GPS_ENABLED = true;
 	public static final boolean ENCODER_ENABLED = true;
-	public static final boolean VISION_SYSTEM_ENABLED = false;
+	public static final boolean VISION_SYSTEM_ENABLED = true;
 	
 	//sensor com ports
 	public static  final String COM_PORT_IMU = "/dev/tty.usbserial-A6026UA0";
