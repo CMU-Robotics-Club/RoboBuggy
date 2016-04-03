88b290e1ada8ffa0cd984ae91f18d7f22552e569
Vivaan Bahl <vivaanbahl@paloaltoapps.com>
2016-04-03 00:40:29 -0400

    Implemented temperature, angular acceleration, linear acceleration, magnetometer, and angular position for Hillcrest IMU

--- real_time/surface_src/java_src/Alice/src/main/java/com/roboclub/robobuggy/nodes/sensors/HillCrestImuNode.java
+++ real_time/surface_src/java_src/Alice/src/main/java/com/roboclub/robobuggy/nodes/sensors/HillCrestImuNode.java
@@ -1,15 +1,13 @@
 package com.roboclub.robobuggy.nodes.sensors;
 
-
 import com.hcrest.jfreespace.Device;
 import com.hcrest.jfreespace.DeviceListenerInterface;
-import com.hcrest.jfreespace.DeviceStatistics;
 import com.hcrest.jfreespace.Discovery;
 import com.hcrest.jfreespace.DiscoveryListenerInterface;
+import com.hcrest.jfreespace.inreport.FreespaceMsgInMotionEngineOutput;
 import com.hcrest.jfreespace.inreport.HidInMsg;
 import com.hcrest.jfreespace.outreport.FreespaceMsgOutDataModeControlV2Request;
 import com.hcrest.jfreespace.outreport.HidOutMsg;
-import com.roboclub.robobuggy.messages.HillCrestIMUMessage;
 import com.roboclub.robobuggy.ros.NodeChannel;
 import com.roboclub.robobuggy.ros.Publisher;
 
@@ -30,14 +28,83 @@ public class HillCrestImuNode implements DiscoveryListenerInterface,DeviceListen
 		Discovery discover = Discovery.getInstance();
 		discover.addListener(this);
 		
+		System.out.println("hillcrest enabled");
+		
 		// TODO Auto-generated constructor stub
 	}
 
+	
+	
 	@Override
 	public void handleRead(Device arg0, HidInMsg arg1, int arg2, long arg3) {
-		// TODO Auto-generated method stub
-		pub.publish(new HillCrestIMUMessage(arg1.toDetailedString()));
-		
+		//TODO handle other kinds of messages
+		if (!(arg1 instanceof FreespaceMsgInMotionEngineOutput)){
+			return;
+		}
+
+		FreespaceMsgInMotionEngineOutput m = (FreespaceMsgInMotionEngineOutput)arg1;
+		int[] data = m.getMeData();
+		//assuming message is of type 0
+		int offset = 0;
+		//we do not parse ff0
+		if(m.getFf0()){
+			offset += 6;
+		}
+		//ff1 is linear acceleration 
+		if(m.getFf1()){
+			double xAccel = convertQNToDouble((byte)data[offset+0],(byte)data[offset+1],10);
+			double yAccel = convertQNToDouble((byte)data[offset+2],(byte)data[offset+3],10);
+			double zAccel = convertQNToDouble((byte)data[offset+4],(byte)data[offset+5],10);
+			//System.out.println("xAccel:"+xAccel+"\tyAccel:"+yAccel+"\tzAccel:"+zAccel);
+			offset +=6;
+		}
+		//ff2 is linear Acceleration no gravity
+		if(m.getFf2()){
+			double xAccel = convertQNToDouble((byte)data[offset+0],(byte)data[offset+1],10);
+			double yAccel = convertQNToDouble((byte)data[offset+2],(byte)data[offset+3],10);
+			double zAccel = convertQNToDouble((byte)data[offset+4],(byte)data[offset+5],10);
+			offset += 6;
+//			System.out.println("xAccel:"+xAccel+"\tyAccel:"+yAccel+"\tzAccel:"+zAccel);
+		}
+		//ff3 is Angular velocity
+		if(m.getFf3()){
+			double xAngularAccel =convertQNToDouble((byte)data[offset+0],(byte)data[offset+1],10);
+			double yAngularAccel = convertQNToDouble((byte)data[offset+2],(byte)data[offset+3],10);
+			double zAngularAccel = convertQNToDouble((byte)data[offset+4],(byte)data[offset+5],10);
+			offset +=6;
+		}
+		//ff4 is magnetometer
+		if(m.getFf4()){
+			double xMag =convertQNToDouble((byte)data[offset+0],(byte)data[offset+1],12);
+			double yMag =convertQNToDouble((byte)data[offset+2],(byte)data[offset+3],12);
+			double zMag =convertQNToDouble((byte)data[offset+4],(byte)data[offset+5],12);
+			offset += 6;
+		}
+		//ff5 is temperature
+		if(m.getFf5()){
+			double temperature = convertQNToDouble((byte)data[offset+0],(byte)data[offset+1],7);
+			System.out.println("temp:"+temperature);
+
+			offset += 2;
+		}
+		//ff6 is angular position
+		if(m.getFf6()){
+			double w = convertQNToDouble((byte)data[offset+0],(byte)data[offset+1],14);
+			double x = convertQNToDouble((byte)data[offset+2],(byte)data[offset+3],14);
+			double y = convertQNToDouble((byte)data[offset+4],(byte)data[offset+5],14);
+			double z = convertQNToDouble((byte)data[offset+6],(byte)data[offset+7],14);
+			
+			//TODO normalize 
+			//extracts 
+			double[][] rot = {
+				{1-2*y*y-2*z*z,      2*x*y-2*z*w,             2*x*z+2*y*w},
+				{2*x*y+2*z*w,        1-2*x*x-2*z*z,           2*y*z-2*x*w},
+				{2*x*z-2*y*w,        2*y*z+2*x*w,            1-2*x*x-2*y*y}
+			};
+
+			
+			offset += 8;
+		}
 	}
 
 	@Override
@@ -60,17 +127,14 @@ public class HillCrestImuNode implements DiscoveryListenerInterface,DeviceListen
 		
 	FreespaceMsgOutDataModeControlV2Request msg = new FreespaceMsgOutDataModeControlV2Request();
 	msg.setPacketSelect(8);  //
-	msg.setModeAndStatus(0);
+	msg.setModeAndStatus(8);
 	msg.setFormatSelect(0);
-	//we really don't know what the format is so we are just going to log everythign for now 
-	msg.setFf0(true);
-	msg.setFf1(true);
-	msg.setFf2(true);
-	msg.setFf3(true);
-	msg.setFf4(true);
-	msg.setFf5(true);
-	msg.setFf6(true);
-	msg.setFf7(true);
+	//we really don't know what the format is so we are just going to log everything for now 
+	msg.setFf2(true); //linear Acceleration without Gravity 
+	msg.setFf3(true); //Angular Velocity
+	msg.setFf4(true); //Magnetometer
+	msg.setFf5(true); //Tempeature
+	msg.setFf6(true); //Angular position
 
 	thisDevice.sendMessageAsync(msg);
 	}
@@ -106,6 +170,28 @@ public class HillCrestImuNode implements DiscoveryListenerInterface,DeviceListen
 		return "hillcrestImu";
 	}
 	
-	
+	public double convertQNToDouble(byte lsb, byte msb,int qn) {
+		// converting Qn fixed point
+		
+		//boolean sign = ((int)msb>>7) == 1;
+		int msbAsInt = ((int)msb) & 0xFF;//0x7F;
+		int lsbAsInt = ((int)lsb & 0xFF);
+		
+		int squashedTogether = ((msbAsInt << 8) | lsbAsInt);
+		Integer sT = new Integer(squashedTogether);
+/*
+		if(sign){
+			sT = -sT;
+		}
+		*/
+		
+		if(sT >= 1<<15){
+			sT = sT - (1<<17-1);
+		}
+		
+		double d = sT.doubleValue();
+		return Math.pow(2, -1 * qn) * d;
+		
+	}
 	
 }
