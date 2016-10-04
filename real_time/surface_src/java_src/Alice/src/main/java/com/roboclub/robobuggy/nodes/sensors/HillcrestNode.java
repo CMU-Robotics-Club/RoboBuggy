466938aaa8c1c26ef2440b5ebe686ef83d3aa953
Vivaan Bahl <vivaanbahl@paloaltoapps.com>
2016-10-04 00:11:01 -0400

    Added configuration for Hillcrest node Need to modify it to support multiple IMUs

--- real_time/surface_src/java_src/Alice/src/main/java/com/roboclub/robobuggy/nodes/sensors/HillcrestNode.java
+++ real_time/surface_src/java_src/Alice/src/main/java/com/roboclub/robobuggy/nodes/sensors/HillcrestNode.java
@@ -1,12 +1,27 @@
 package com.roboclub.robobuggy.nodes.sensors;
 
+import com.hcrest.jfreespace.Device;
+import com.hcrest.jfreespace.DeviceListenerInterface;
+import com.hcrest.jfreespace.Discovery;
+import com.hcrest.jfreespace.DiscoveryListenerInterface;
+import com.hcrest.jfreespace.FreespaceErrorCodes;
+import com.hcrest.jfreespace.inreport.HidInMsg;
+import com.hcrest.jfreespace.outreport.FreespaceMsgOutDataModeControlV2Request;
+import com.hcrest.jfreespace.outreport.HidOutMsg;
+import com.roboclub.robobuggy.main.RobobuggyLogicNotification;
+import com.roboclub.robobuggy.main.RobobuggyMessageLevel;
+import com.roboclub.robobuggy.messages.ImuMeasurement;
 import com.roboclub.robobuggy.nodes.baseNodes.BuggyDecoratorNode;
 import com.roboclub.robobuggy.nodes.baseNodes.BuggyNode;
+import com.roboclub.robobuggy.ros.NodeChannel;
+import com.roboclub.robobuggy.ros.Publisher;
 
 /**
  * A node to communicate with the Hillcrest Freespace 9DOF IMU
  */
-public class HillcrestNode extends BuggyDecoratorNode {
+public class HillcrestNode extends BuggyDecoratorNode implements DeviceListenerInterface, DiscoveryListenerInterface {
+
+    private Device hillcrestImu;
 
     /**
      * Creates a new decorator for the given {@link Node}
@@ -16,15 +31,71 @@ public class HillcrestNode extends BuggyDecoratorNode {
      */
     public HillcrestNode(BuggyNode node, String name) {
         super(node, name);
+
     }
 
     @Override
     protected boolean startDecoratorNode() {
-        return false;
+        // look for an IMU, and add this as a listener
+        Discovery.getInstance().addListener(this);
+
+        return true;
     }
 
     @Override
     protected boolean shutdownDecoratorNode() {
-        return false;
+        Discovery.getInstance().removeListener(this);
+        return true;
+    }
+
+    @Override
+    public void handleRead(Device device, HidInMsg hidInMsg, int i, long l) {
+
+    }
+
+    @Override
+    public void handleSend(Device device, HidOutMsg hidOutMsg, int i) {
+
+    }
+
+    @Override
+    public void notifyRemoved(Device device) {
+
+    }
+
+    @Override
+    public void freespaceDeviceInserted(Device device) {
+        hillcrestImu = device;
+        hillcrestImu.open(this);
+
+        int modeAndStatus = 8; // bit 3 set gives Full Motion On mode - page 41
+        int packetSelect  = 8; // 8 = MotionEngineOutput              - page 41
+        int formatSelect  = 1; // Mode 1                              - page 18
+        FreespaceMsgOutDataModeControlV2Request configMsg = new FreespaceMsgOutDataModeControlV2Request();
+
+        configMsg.setModeAndStatus(modeAndStatus);
+        configMsg.setPacketSelect(packetSelect);
+        configMsg.setFormatSelect(formatSelect);
+
+        // get all the data
+        configMsg.setFf0(true);
+        configMsg.setFf1(true);
+        configMsg.setFf2(true);
+        configMsg.setFf3(true);
+        configMsg.setFf4(true);
+        configMsg.setFf5(true);
+        configMsg.setFf6(true);
+        configMsg.setFf7(true);
+
+        FreespaceErrorCodes response = hillcrestImu.sendMessage(configMsg);
+        if (!response.equals(FreespaceErrorCodes.FREESPACE_SUCCESS)) {
+            new RobobuggyLogicNotification("Error configuring IMU!", RobobuggyMessageLevel.EXCEPTION);
+        }
+    }
+
+    @Override
+    public void freespaceDeviceRemoved(Device device) {
+        hillcrestImu.close();
+        hillcrestImu = null;
     }
 }
