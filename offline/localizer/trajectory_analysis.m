17b176e473a9ad06010131258ffc33533236576f
Bereket Abraham <babraham42@gmail.com>
2016-11-18 18:07:00 -0500

    fixed loclizer trajectory analysis

--- offline/localizer/trajectory_analysis.m
+++ offline/localizer/trajectory_analysis.m
@@ -7,18 +7,18 @@ addpath('./latlonutm/Codes/matlab');
 % addpath('./zoharby-plot_google_map');
 addpath('./altmany-export_fig');
 
-file = 'localizer_v3.mat';
+file = 'localizer_v4.mat';
 load(file, 'trajectory', 'P');
 save_plot = false;
 
-k = 1000;
-wmline(trajectory(8,1:k:end), trajectory(9,1:k:end), 'Color', 'r')
+k = 100;
+wmline(trajectory(6,1:k:end), trajectory(7,1:k:end), 'Color', 'r')
 
 t1 = 1476008235676;
 t2 = 1476009019782;
 t = linspace(0, (t2 - t1)/1000, size(trajectory,2));
 figure();
-plot(t, trajectory(10,:))
+plot(t, trajectory(8,:))
 title(['Uncertainty ', file]);
 
 % plot(trajectory(8,:), trajectory(9,:), '.r', 'MarkerSize', 20) 
@@ -29,14 +29,14 @@ if save_plot
     export_fig('out.jpg')
 end
 
-theta = deg2rad( trajectory(5,1:k:end) );
+heading = trajectory(4,1:k:end);
 figure();
 hold on;
 plot(trajectory(1,1:k:end), trajectory(2,1:k:end))
-quiver(trajectory(1,1:k:end), trajectory(2,1:k:end), cos(theta), sin(theta))
+quiver(trajectory(1,1:k:end), trajectory(2,1:k:end), cos(heading), sin(heading))
 hold off;
 title(['Heading ', file]);
 
 % plot on google maps
-ss = [trajectory(8,1:k:end); trajectory(9,1:k:end)];
+ss = [trajectory(6,1:k:end); trajectory(7,1:k:end)];
 fprintf(1, '%5.20f, %5.20f\n', ss);
