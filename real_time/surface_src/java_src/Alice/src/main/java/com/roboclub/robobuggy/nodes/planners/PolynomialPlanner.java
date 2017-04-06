bbccdb25b64aac0deddd29420f4dce2ca75d8b65
Vivaan Bahl <vivaanbahl@paloaltoapps.com>
2017-04-06 01:53:15 -0400

    added mroe to the polynomial planner

--- real_time/surface_src/java_src/Alice/src/main/java/com/roboclub/robobuggy/nodes/planners/PolynomialPlanner.java
+++ real_time/surface_src/java_src/Alice/src/main/java/com/roboclub/robobuggy/nodes/planners/PolynomialPlanner.java
@@ -5,6 +5,7 @@ import com.roboclub.robobuggy.messages.GpsMeasurement;
 import com.roboclub.robobuggy.ros.NodeChannel;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by vivaanbahl on 4/4/17.
@@ -12,6 +13,18 @@ import java.util.ArrayList;
 public class PolynomialPlanner extends PathPlannerNode {
 
     private ArrayList<GpsMeasurement> waypoints;
+    private Date expirationTime;
+    private double previousSteeringAngle;
+    private GPSPoseMessage currentPose;
+    private GpsMeasurement target;
+    private ArrayList<GpsMeasurement> checkInPoints;
+
+    // switch checkin points if we are 2m away from the current target checkin
+    private static final int SWITCHING_TOLERANCE_M = 2;
+    // recompute the polynomial every 1000ms
+    private static final int POLYNOMIAL_RECOMPUTE_PERIOD = 1000;
+    // sample the polynomial to get checkin points every 50 ms along the curve
+    private static final int CHECKIN_SAMPLE_PERIOD = 50;
 
     /**
      * Construct a new {@link PathPlannerNode}
@@ -20,15 +33,52 @@ public class PolynomialPlanner extends PathPlannerNode {
     public PolynomialPlanner(ArrayList<GpsMeasurement> waypoints) {
         super(NodeChannel.PATH_PLANNER);
         this.waypoints = waypoints;
+        previousSteeringAngle = 0;
+        expirationTime = new Date();
     }
 
     @Override
     protected void updatePositionEstimate(GPSPoseMessage m) {
 
+        currentPose = m;
+
     }
 
     @Override
     protected double getCommandedSteeringAngle() {
+        Date currentTime = new Date();
+        long elapsedMilliseconds = expirationTime.getTime() - currentTime.getTime();
+
+        // if we've expired the current polynomial curve either in time or in space recompute
+        if (elapsedMilliseconds < 0 || checkInPoints.size() == 0) {
+
+            ArrayList<GPSPoseMessage> samplePoints = new ArrayList<>();
+            samplePoints.add(currentPose);
+
+            // get all the waypoints within the recompute period away from us (velocity times period)
+            double lookaheadDist = currentPose.getCurrentState().get(2, 0) * POLYNOMIAL_RECOMPUTE_PERIOD;
+            int lookaheadIndex = 0;
+            for (GpsMeasurement candidate : waypoints) {
+                if (GPSPoseMessage.getDistance(currentPose, candidate.toGpsPoseMessage(0)) < lookaheadDist) {
+                    samplePoints.add(candidate.toGpsPoseMessage(0));
+                }
+            }
+
+            // build our regression fit matrix
+            double[] yDouble = new double[samplePoints.size()];
+            double[][] xDouble = new double[samplePoints.size()][samplePoints.size()];
+            // TODO: 4/6/17 fill this in tomorrow
+
+        }
+        // otherwise we still should be following this curve, check whether we're too close to this current check-in point
+        else {
+            GPSPoseMessage targetAsPose = target.toGpsPoseMessage(0);
+            double targetDistance = GPSPoseMessage.getDistance(currentPose, targetAsPose);
+            if (targetDistance <= SWITCHING_TOLERANCE_M) {
+                target = checkInPoints.remove(0);
+            }
+        }
+
         return 0;
     }
 
