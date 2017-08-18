fc7c1cc4197fbe600236e0e3c4cf9867b7b963fc
Vivaan Bahl <vivaanbahl@gmail.com>
2017-08-18 01:38:18 -0700

    Added in a dummy encoder node

--- 2017-2018/ROS_RoboBuggy/src/robobuggy/src/Controller.cpp
+++ 2017-2018/ROS_RoboBuggy/src/robobuggy/src/Controller.cpp
@@ -1,3 +1,4 @@
+#include <robobuggy/ENC.h>
 #include "ros/ros.h"
 #include "robobuggy/IMU.h"
 #include "robobuggy/GPS.h"
@@ -12,6 +13,11 @@ void GPS_Callback(const robobuggy::GPS::ConstPtr& msg)
     ROS_INFO("Received GPS message with coordinates: (%fm, %fm)", msg->Lat_m, msg->Long_m);
 }
 
+void ENC_Callback(const robobuggy::ENC::ConstPtr& msg)
+{
+    ROS_INFO("Received ENC message with coordinates: (%fm, %fm, %fm, %fm)", msg->distance_m, msg->velocity_ms, msg->acceleration_mss, msg->raw_data_word);
+}
+
 int main(int argc, char **argv) 
 {
     ros::init(argc, argv, "Controller");
@@ -20,6 +26,7 @@ int main(int argc, char **argv)
 
     ros::Subscriber imu_sub = nh.subscribe<robobuggy::IMU>("IMU", 1000, IMU_Callback);
     ros::Subscriber gps_sub = nh.subscribe<robobuggy::GPS>("GPS", 1000, GPS_Callback);
+    ros::Subscriber enc_sub = nh.subscribe<robobuggy::ENC>("ENC", 1000, ENC_Callback);
 
     ros::spin();
 
