e67198b6185d82ed4b66030c1b6158d13774f5c0
Vivaan Bahl <vivaanbahl@gmail.com>
2017-09-26 15:12:29 -0400

    Added matrix library to localizer

--- Software/real_time/ROS_RoboBuggy/src/robobuggy/include/localizer/Localizer.h
+++ Software/real_time/ROS_RoboBuggy/src/robobuggy/include/localizer/Localizer.h
@@ -1,6 +1,13 @@
 #include "ros/ros.h"
 
 #include <stdio.h>
+#include <robobuggy/IMU.h>
+#include <robobuggy/GPS.h>
+#include <robobuggy/ENC.h>
+
+#include <eigen3/Eigen/Dense>
+
+using Eigen::MatrixXd;
 
 class Localizer
 {
@@ -10,15 +17,25 @@ public:
     static void GPS_Callback(const robobuggy::GPS::ConstPtr& msg);
     static void ENC_Callback(const robobuggy::ENC::ConstPtr& msg);
 private:
-    void update_position_estimate();
-    ros::NodeHandle nh;
-    ros::Publisher pose_pub;
-    ros::Subscriber gps_pub;
-    ros::Subscriber imu_pub;
-    ros::Subscriber enc_pub;
     double current_gps_lat;
     double current_gps_lon;
     double current_encoder_ticks;
     double prev_encoder_ticks;
     double current_imu_heading;
+
+    ros::NodeHandle nh;
+    ros::Publisher pose_pub;
+    ros::Subscriber gps_sub;
+    ros::Subscriber imu_sub;
+    ros::Subscriber enc_sub;
+
+    MatrixXd motion_model;
+    MatrixXd R;
+    MatrixXd P;
+    MatrixXd Q_GPS;
+    MatrixXd Q_Encoder;
+    MatrixXd C_GPS;
+    MatrixXd C_Encoder;
+
+    void update_position_estimate();
 };
--- Software/real_time/ROS_RoboBuggy/src/robobuggy/src/localizer/Localizer.cpp
+++ Software/real_time/ROS_RoboBuggy/src/robobuggy/src/localizer/Localizer.cpp
@@ -1,16 +1,16 @@
 #include "localizer/Localizer.h"
 
-static void Localizer::ENC_Callback(const robobuggy::ENC:ConstPtr& msg)
+void Localizer::ENC_Callback(const robobuggy::ENC::ConstPtr &msg)
 {
 
 }
 
-static void Localizer::GPS_Callback(const robobuggy::GPS::ConstPtr& msg)
+void Localizer::GPS_Callback(const robobuggy::GPS::ConstPtr &msg)
 {
 
 }
 
-static void Localizer::IMU_Callback(const robobuggy::IMU::ConstPtr& msg)
+void Localizer::IMU_Callback(const robobuggy::IMU::ConstPtr &msg)
 {
 
 }
@@ -20,9 +20,16 @@ Localizer::Localizer()
     imu_sub = nh.subscribe<robobuggy::IMU>("IMU", 1000, IMU_Callback);
     gps_sub = nh.subscribe<robobuggy::GPS>("GPS", 1000, GPS_Callback);
     enc_sub = nh.subscribe<robobuggy::ENC>("ENC", 1000, ENC_Callback);
+
+    R = MatrixXd(5,5);
 }
 
 void Localizer::update_position_estimate()
 {
+
+}
+
+// TODO create runner file once we've filled the rest of this out
+int main() {
 
 }
