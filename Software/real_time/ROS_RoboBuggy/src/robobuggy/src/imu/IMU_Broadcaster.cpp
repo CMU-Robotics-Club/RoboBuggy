c88fe9cb23e216bd4445b5e977b358ee7ce6a1db
Vivek Sridhar <vivek4830@gmail.com>
2017-09-30 15:06:24 -0700

    Started writing IMU node

--- Software/real_time/ROS_RoboBuggy/src/robobuggy/src/imu/IMU_Broadcaster.cpp
+++ Software/real_time/ROS_RoboBuggy/src/robobuggy/src/imu/IMU_Broadcaster.cpp
@@ -1,24 +1,43 @@
 #include "imu/IMU_Broadcaster.h"
 
-float IMU_Broadcaster::get_spoofed_x_accel()
-{
-    return 1.5;
-}
-float IMU_Broadcaster::get_spoofed_y_accel()
-{
-    return 2.5;
-}
-float IMU_Broadcaster::get_spoofed_z_accel()
-{
-    return 1.5;
-}
-
 IMU_Broadcaster::IMU_Broadcaster()
 {
     imu_pub = nh.advertise<robobuggy::IMU>("IMU", 1000);
 }
 
-void IMU_Broadcaster::publish_new_spoofed_message()
+void IMU_Broadcaster::init_IMU()
+{
+    int err;
+    err = freespace_init();
+    if (err != FREESPACE_SUCCESS) {
+        ROS_ERROR_STREAM("libfreespace initialization error %d\n", err);
+        return err;
+    }
+
+    err = freespace_getDeviceList(&device, 1, &numIds);
+    if (numIds == 0) {
+        //@TODO: Keep trying to locate IMU
+        ROS_INFO_STREAM("Didn't find any devices\n");
+    }
+
+    err = freespace_openDevice(device);
+    if (err != FREESPACE_SUCCESS) {
+        ROS_ERROR_STREAM("Error opening device\n");
+        return err;
+    }
+
+    err = freespace_flush(device);
+    if (err != FREESPACE_SUCCESS) {
+        ROS_ERROR_STREAM("Error flushing device\n");
+        return err;
+    }
+
+    // Configure device for motion outputs
+    message.messageType = FREESPACE_MESSAGE_DATAMODECONTROLV2REQUEST;
+    
+}
+
+void IMU_Broadcaster::publish_IMU_messages()
 {
     robobuggy::IMU msg;
 
