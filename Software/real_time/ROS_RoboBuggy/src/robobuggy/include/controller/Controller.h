ed6faf769f838450a5e34ab48a8384a403dded49
Vivaan Bahl <vivaanbahl@gmail.com>
2017-10-13 16:34:36 -0400

    added in rest of the ported controller

--- Software/real_time/ROS_RoboBuggy/src/robobuggy/include/controller/Controller.h
+++ Software/real_time/ROS_RoboBuggy/src/robobuggy/include/controller/Controller.h
@@ -5,17 +5,18 @@
 #ifndef ROS_ROBOBUGGY_CONTROLLER_H
 #define ROS_ROBOBUGGY_CONTROLLER_H
 
-#include <robobuggy/ENC.h>
 #include "ros/ros.h"
-#include "robobuggy/IMU.h"
-#include "robobuggy/GPS.h"
-#include "robobuggy/Pose.h"
-#include "std_msgs/String.h"
+#include <robobuggy/Pose.h>
+#include <robobuggy/Steering.h>
+#include <robobuggy/GPS.h>
 #include <string>
+#include <geodesy/utm.h>
+#include <cmath>
+
 class Controller
 {
 public:
-    static void Pose_Callback(const robobuggy::Pose::ConstPtr& msg);
+    void Pose_Callback(const robobuggy::Pose::ConstPtr& msg);
     Controller();
     ros::NodeHandle nh;
     void update_steering_estimate();
@@ -32,6 +33,9 @@ private:
     int get_closest_waypoint_index();
     double pure_pursuit_controller();
     bool get_deploy_brake_value();
+    double normalize_angle_rad();
+    double get_distance_from_pose(robobuggy::Pose pose_msg);
+    double normalize_angle_rad(double radians);
 };
 
 #endif //ROS_ROBOBUGGY_CONTROLLER_H
--- Software/real_time/ROS_RoboBuggy/src/robobuggy/src/controller/Controller.cpp
+++ Software/real_time/ROS_RoboBuggy/src/robobuggy/src/controller/Controller.cpp
@@ -2,17 +2,33 @@
 
 void Controller::Pose_Callback(const robobuggy::Pose::ConstPtr& msg)
 {
+    current_pose_estimate.heading_rad = msg->heading_rad;
+    current_pose_estimate.latitude_deg = msg->latitude_deg;
+    current_pose_estimate.longitude_deg = msg->longitude_deg;
 }
 
 
 Controller::Controller()
 {
-
+    steering_pub = nh.advertise<robobuggy::Steering>("Steering_Command", 1000);
+    pose_sub = nh.subscribe<robobuggy::Pose>("Pose", 1000, &Controller::Pose_Callback, this);
 }
 
 void Controller::update_steering_estimate()
 {
+    robobuggy::Steering steering_msg;
+    steering_msg.steer_feedback = 0;
+    steering_msg.steer_angle = pure_pursuit_controller();
 
+    steering_pub.publish(steering_msg);
+}
+
+double Controller::get_distance_from_pose(robobuggy::Pose pose_msg)
+{
+    double dx = (current_pose_estimate.longitude_deg - pose_msg.longitude_deg) * 84723.58765;
+    double dy = (current_pose_estimate.latitude_deg - pose_msg.latitude_deg) * 111319.9;
+
+    return std::sqrt(dx * dx + dy * dy);
 }
 
 int Controller::get_closest_waypoint_index()
@@ -26,10 +42,7 @@ int Controller::get_closest_waypoint_index()
         gpsPoseMessage.latitude_deg = waypoint_list.at(i).Lat_deg;
         gpsPoseMessage.longitude_deg = waypoint_list.at(i).Long_deg;
 
-        double dx = (current_pose_estimate.longitude_deg - gpsPoseMessage.longitude_deg) * 84723.58765;
-        double dy = (current_pose_estimate.latitude_deg - gpsPoseMessage.latitude_deg) * 111319.9;
-
-        double d = std::sqrt(dx * dx + dy * dy);
+        double d = get_distance_from_pose(gpsPoseMessage);
 
         if (d < min) {
             min = d;
@@ -42,10 +55,70 @@ int Controller::get_closest_waypoint_index()
 
 double Controller::pure_pursuit_controller()
 {
+    int closest_index = get_closest_waypoint_index();
+    double k = 2.5;
+    double velocity = 3; // TODO bake velocity into pose messages
+    double lookahead_lower_bound = 5.0;
+    double lookahead_upper_bound = 25.0;
+    double lookahead = (k*velocity) / 2.0;
 
+    if(lookahead < lookahead_lower_bound) 
+    {
+        lookahead = lookahead_lower_bound;
+    }
+    else if(lookahead > lookahead_upper_bound) 
+    {
+        lookahead = lookahead_upper_bound;
+    }
+
+    int lookahead_index = 0;
+    for (lookahead_index = closest_index; lookahead_index < waypoint_list.size(); lookahead_index++)
+    {
+        robobuggy::Pose gpsPoseMessage;
+        gpsPoseMessage.latitude_deg = waypoint_list.at(lookahead_index).Lat_deg;
+        gpsPoseMessage.longitude_deg = waypoint_list.at(lookahead_index).Long_deg;
+
+        if (get_distance_from_pose(gpsPoseMessage) < lookahead)
+        {
+            break;
+        }
+    }
+
+    robobuggy::GPS target_waypoint = waypoint_list.at(lookahead_index);
+    
+    geographic_msgs::GeoPoint gps_point;
+    gps_point.latitude = target_waypoint.Lat_deg;
+    gps_point.longitude = target_waypoint.Long_deg;
+
+    geodesy::UTMPoint utm_waypoint(gps_point);
+
+    gps_point.latitude = current_pose_estimate.latitude_deg;
+    gps_point.longitude = current_pose_estimate.longitude_deg;
+    geodesy::UTMPoint utm_pose(gps_point);
+
+    double dx = utm_waypoint.easting - utm_pose.easting;
+    double dy = utm_waypoint.northing - utm_pose.northing;
+    double theta = atan2(dy, dx) - current_pose_estimate.heading_rad;
+
+    double commanded_angle = normalize_angle_rad(atan2(2 * 1.13 * sin(theta), lookahead));
+    return commanded_angle;
 }
 
 bool Controller::get_deploy_brake_value()
 {
     return false;
+}
+
+double Controller::normalize_angle_rad(double radians) {
+    while (radians < 0.0) 
+    {
+        radians = radians + 2 * M_PI;
+    }
+
+    if (radians > M_PI) 
+    {
+        radians = radians - 2.0 * M_PI;
+    }
+    return radians;
+
 }
