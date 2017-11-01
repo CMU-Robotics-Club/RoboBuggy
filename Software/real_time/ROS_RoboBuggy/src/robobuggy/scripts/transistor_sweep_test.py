7212e35f357e1feab84ea5889f1e3687c7c9a238
Vivek Sridhar <vivek4830@gmail.com>
2017-11-01 15:22:15 -0400

    Added steering sweep node

---
+++ Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/transistor_sweep_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import rospy
+from robobuggy.msg import Command
+
+def sweep_steering():
+    pub = rospy.Publisher('Command', Command, queue_size=1000)
+    rospy.init_node('steering_sweep', anonymous=True)
+    rate = rospy.Rate(1) # 1Hz
+
+    steering_pos = -1500
+    steering_change = 100
+    command_msg = Command()
+    while not rospy.is_shutdown():
+        command_msg.steer_cmd = steering_pos
+        command_msg.brake_cmd = False
+        pub.publish(command_msg)
+        steering_pos += steering_change
+        if (steering_pos == 1500):
+            steering_change = -100
+        if (steering_pos == -1500):
+            steering_change = 100
+
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        sweep_steering()
+    except rospy.ROSInterruptException:
+        pass
