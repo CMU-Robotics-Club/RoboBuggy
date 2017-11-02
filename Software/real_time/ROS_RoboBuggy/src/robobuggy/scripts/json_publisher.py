d0f77070bae9af0308c9260637edd88cb19c4d80
Pranav Kumar <pmkumar@andrew.cmu.edu>
2017-11-02 12:18:45 -0400

    Add JSON Publishing for GPS and Encoder and add to launch file

--- Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/json_publisher.py
+++ Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/json_publisher.py
@@ -2,19 +2,27 @@
 
 import rospy
 import json
-from robobuggy.msg import ENC
+from robobuggy.msg import ENC, GPS
 import time
 import requests
 
-def subscriber_callback(data):
+def subscriber_callback_ENC(data):
     tick = data.ticks
     json_dict = {"ticks" : tick}
     request = requests.post('https://robobuggy-web-server.herokuapp.com/encoderData', json = json_dict)
+    print("Published to Encoder")
+    pass
+
+def subscriber_callback_GPS(data):
+    json_dict = {"latitude" : data.Lat_deg, "longitude" : data.Lon_deg}
+    request = requests.post('https://robobuggy-web-server.herokuapp.com/gpsData', json = json_dict)
+    print("Published to GPS")
     pass
 
 def start_subscriber_spin():
     rospy.init_node("JSON_Publisher", anonymous=True)
-    rospy.Subscriber("Encoder", ENC, subscriber_callback)
+    rospy.Subscriber("Encoder", ENC, subscriber_callback_ENC)
+    rospy.Subscriber("GPS", GPS, subscriber_callback_GPS)
     print("Success!")
     rospy.spin()
     pass
