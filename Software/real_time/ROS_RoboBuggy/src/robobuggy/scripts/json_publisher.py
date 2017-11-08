74fe84ba1db169d9843a1baaf55603fc6f113da4
= <pmkumar@andrew.cmu.edu>
2017-11-08 16:38:59 -0500

    Created common dictionary for packets

--- Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/json_publisher.py
+++ Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/json_publisher.py
@@ -9,21 +9,26 @@ import requests
 diag_json_dict = { "batteryLevel" : -1 }
 enc_json_dict = { "ticks" : -1 }
 gps_json_dict = { "latitude" : -1, "longitude" : -1 }
+data_dict = { "batteryLevel" : -1, "ticks" : -1, "latitude" : -1, "longitude" : -1 }
 
 def subscriber_callback_Diagnostics(data):
-    global diag_json_dict
+    global diag_json_dict, data_dict
     battery_level = data.battery_level
     diag_json_dict = {"batteryLevel" : battery_level}
+    data_dict["batteryLevel"] = battery_level
     pass
 def subscriber_callback_ENC(data):
     global enc_json_dict
     tick = data.ticks
     enc_json_dict = {"ticks" : tick}
+    data_dict["ticks"] = tick
     pass
 
 def subscriber_callback_GPS(data):
     global gps_json_dict
     gps_json_dict = {"latitude" : data.Lat_deg, "longitude" : data.Long_deg}
+    data_dict["latitude"] = data.Lat_deg
+    data_dict["longitude"] = data.Long_deg
     pass
 
 def start_subscriber_spin():
@@ -34,10 +39,10 @@ def start_subscriber_spin():
 
     rate = rospy.Rate(0.5)
     while not rospy.is_shutdown():
-        request = requests.post('https://robobuggy-web-server.herokuapp.com/gpsData', json = gps_json_dict)
-        request = requests.post('https://robobuggy-web-server.herokuapp.com/encoderData', json = enc_json_dict)
-        request = requests.post('https://robobuggy-web-server.herokuapp.com/diagnosticsData', json = diag_json_dict)
-
+        #request = requests.post('https://robobuggy-web-server.herokuapp.com/gpsData', json = gps_json_dict)
+        #request = requests.post('https://robobuggy-web-server.herokuapp.com/encoderData', json = enc_json_dict)
+        #request = requests.post('https://robobuggy-web-server.herokuapp.com/diagnosticsData', json = diag_json_dict)
+        request = requests.post('https:robobuggy-web-server.herokuapp.com/data', json = data_dict)
         rate.sleep()
 
 
