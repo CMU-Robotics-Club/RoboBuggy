daa24b46c2d37ddb11426ea06dd79f6d90c034f0
Abhinav Girish <abhinav.girish97@gmail.com>
2018-01-26 19:19:53 -0500

    first mapviz simulation

--- Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/data_map_plot.py
+++ Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/data_map_plot.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 
 import rospy
+from gps_common.msg import GPSFix
 from robobuggy.msg import GPS
 
+viz_pub = None;
+
 def subscriber_callback(data):
-    rospy.loginfo("got gps msg: %f, %f", data.Lat_deg, data.Lon_deg)
+    global viz_pub;
+    rospy.loginfo("got gps msg: %f, %f", data.Lat_deg, data.Long_deg)
+
+    viz_msg = GPSFix(latitude=data.Lat_deg, longitude=data.Long_deg);
+    viz_pub.publish(viz_msg);
+
     pass
 
 def start_subscriber_spin():
-
+    global viz_pub
     rospy.init_node("GPS_Plotter", anonymous=True)
+    viz_pub = rospy.Publisher('GPS_VIZ', GPSFix, queue_size=10);
     rospy.Subscriber("GPS", GPS, subscriber_callback)
 
     rospy.spin()
@@ -18,3 +27,4 @@ def start_subscriber_spin():
 
 if __name__ == "__main__":
     start_subscriber_spin()
+
