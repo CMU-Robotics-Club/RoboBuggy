fbedb30e06ba75b1ce375316e1020a08e31ffbc7
HaoranFei <hardyfeihaoran@126.com>
2018-01-29 16:57:56 -0800

    data_map_plot.py is updated Now subscribes to Pose instead of GPS Now should correctly display orientation data from heading_rad Still no feature for steering data Will add

--- Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/data_map_plot.py
+++ Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/data_map_plot.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 
 import rospy
+import math
 from gps_common.msg import GPSFix
 from robobuggy.msg import GPS
+from robobuggy.msg import Pose
 
 viz_pub = None;
 
 def subscriber_callback(data):
     global viz_pub;
-    rospy.loginfo("got gps msg: %f, %f", data.Lat_deg, data.Long_deg)
+    rospy.loginfo("got Pose msg: %f degrees lat, %f degrees long, %f rad", data.latitude_deg, data.longitude_deg, data.heading_rad)
 
-    viz_msg = GPSFix(latitude=data.Lat_deg, longitude=data.Long_deg);
-    viz_pub.publish(viz_msg);
+    #data.heading_rad is in Radians from north clockwise, but GPSFix requires degrees from north
+    float64 degrees_from_north = 180 * data.heading_rad / math.pi
+    
+    viz_msg = GPSFix(latitude=data.latitude_deg, longitude=data.longitude_deg, track = degrees_from_north)
+    viz_pub.publish(viz_msg)
 
     pass
 
@@ -19,7 +24,7 @@ def start_subscriber_spin():
     global viz_pub
     rospy.init_node("GPS_Plotter", anonymous=True)
     viz_pub = rospy.Publisher('GPS_VIZ', GPSFix, queue_size=10);
-    rospy.Subscriber("GPS", GPS, subscriber_callback)
+    rospy.Subscriber("Pose", Pose, subscriber_callback)
 
     rospy.spin()
 
