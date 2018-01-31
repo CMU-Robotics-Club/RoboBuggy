4bb5ff6af9b483c0a6a94feff6826d5bfd5ec531
Abhinav Girish <abhinav.girish97@gmail.com>
2018-01-31 18:00:48 -0500

    adding in steering command subscriber

--- Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/data_map_plot.py
+++ Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/data_map_plot.py
@@ -14,9 +14,13 @@ def subscriber_callback(data):
 
     #data.heading_rad is in Radians from north clockwise, but GPSFix requires degrees from north
     float64 degrees_from_north = 180 * data.heading_rad / math.pi
-    
-    viz_msg = GPSFix(latitude=data.latitude_deg, longitude=data.longitude_deg, track = degrees_from_north)
-    viz_pub.publish(viz_msg)
+    float64 steering_angle = 180 * data.steer_cmd / math.pi
+   
+    viz_msg_heading = GPSFix(latitude=data.latitude_deg, longitude=data.longitude_deg, track = degrees_from_north)
+    viz_msg_steering = GPSFix(latitude=data.latitude_deg, longitude=data.longitude_deg, track = degrees_from_north + steering_angle)
+ 
+    viz_pub.publish(viz_msg_heading)
+    viz_pub.publish(viz_msg_steering)
 
     pass
 
@@ -25,6 +29,7 @@ def start_subscriber_spin():
     rospy.init_node("GPS_Plotter", anonymous=True)
     viz_pub = rospy.Publisher('GPS_VIZ', GPSFix, queue_size=10);
     rospy.Subscriber("Pose", Pose, subscriber_callback)
+    rospy.Subscriber("Command", Command, subscriber_callback)
 
     rospy.spin()
 
