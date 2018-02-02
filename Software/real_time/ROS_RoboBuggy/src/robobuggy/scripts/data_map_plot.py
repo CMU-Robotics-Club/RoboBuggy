a1aa316865d9b02528228cb1d3e1a8d0c0efc8b0
HaoranFei <hardyfeihaoran@126.com>
2018-02-01 19:41:39 -0800

    Updated translator by fixing the error Original error is having one callback for more than one message type See Slack private chat for more info

--- Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/data_map_plot.py
+++ Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/data_map_plot.py
@@ -8,28 +8,47 @@ from robobuggy.msg import Pose
 
 viz_pub = None;
 
-def subscriber_callback(data):
-    global viz_pub;
+def pose_callback(data):
+    global viz_pub
+
     rospy.loginfo("got Pose msg: %f degrees lat, %f degrees long, %f rad", data.latitude_deg, data.longitude_deg, data.heading_rad)
 
     #data.heading_rad is in Radians from north clockwise, but GPSFix requires degrees from north
     float64 degrees_from_north = 180 * data.heading_rad / math.pi
-    float64 steering_angle = 180 * data.steer_cmd / math.pi
-   
+
+    last_latitude = data.latitude_deg
+    last_longitude = data.longitude_deg
+    last_heading = degrees_from_north
+    
     viz_msg_heading = GPSFix(latitude=data.latitude_deg, longitude=data.longitude_deg, track = degrees_from_north)
-    viz_msg_steering = GPSFix(latitude=data.latitude_deg, longitude=data.longitude_deg, track = degrees_from_north + steering_angle)
  
     viz_pub.publish(viz_msg_heading)
+
+    pass
+
+def command_callback(data):
+    global viz_pub
+    
+    #steer_cmd should be degrees
+    rospy.loginfo("got Steering Command msg: %f degrees", data.steer_cmd)
+
+    viz_msg_steering = GPSFix(latitude=last_latitude, longitude=last_longitude, track = last_heading + data.steer_cmd)
+ 
     viz_pub.publish(viz_msg_steering)
 
     pass
 
 def start_subscriber_spin():
     global viz_pub
+
+    global float64 last_latitude
+    global float64 last_longitude
+    global float64 last_heading
+    
     rospy.init_node("GPS_Plotter", anonymous=True)
     viz_pub = rospy.Publisher('GPS_VIZ', GPSFix, queue_size=10);
-    rospy.Subscriber("Pose", Pose, subscriber_callback)
-    rospy.Subscriber("Command", Command, subscriber_callback)
+    rospy.Subscriber("Pose", Pose, pose_callback)
+    rospy.Subscriber("Command", Command, command_callback)
 
     rospy.spin()
 
