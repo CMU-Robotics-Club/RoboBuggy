24516b16dae78015b8bda47aa640ce4ea1bed2d9
HaoranFei <hardyfeihaoran@126.com>
2018-03-05 12:58:18 -0800

    UTM conversion added. Should be finished now.

--- Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/controller_simulator.py
+++ Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/controller_simulator.py
@@ -18,6 +18,10 @@ global rate = rospy.Rate(10)
 #subject to change in the future
 global init_latitude = 40.4406
 global init_longitude = -79.9959
+global utm_zone_number = 17
+global utm_zone_letter = 'T'
+
+
 
 def updateModel(x):
     matrix = 
@@ -29,10 +33,6 @@ def updateModel(x):
         [0, 0, math.tan(steering)/wheelbase, 0, 0]
     ]
     return matrix
-
-#need to implement conversion with UTM library
-
-
 
 def command_callback(data):
     
@@ -48,10 +48,18 @@ def command_callback(data):
     pass
 
 def initialize():
+
     #set up the initial position
+    #Syntax see python package index
+    #The syntax is utm.from_latlon(LATITUDE, LONGITUDE).
+    #The return has the form (EASTING, NORTHING, ZONE NUMBER, ZONE LETTER).
+
+    utm = utm.from_latlon(init_latitude, init_longitude)
     x = [
-            [], 
-            [northing], 
+            #UTM Easting
+            [utm[0]], 
+            #UTM Northing
+            [utm[1]], 
             [1], 
             [250 * math.pi / 180], 
             [0]
@@ -75,9 +83,14 @@ def start_simulator_spin():
         #publish a new pose message using simulator_pub
         simulated_pose = Pose()
 
-        #This needs some conversion fixes
-        simulated_pose.latitude_deg = northingToLatitude(x[1][0])
-        simulated_pose.longitude_deg = eastingToLongitude(x[0[0]])
+        #lat_long is a 2-tuple
+        #for syntax of the utm package see python package index
+        #The syntax is utm.to_latlon(EASTING, NORTHING, ZONE NUMBER, ZONE LETTER).
+        #The return has the form (LATITUDE, LONGITUDE).
+        
+        lat_long = utm.to_latlon(x[0][0], x[1][0], utm_zone_number, utm_zone_letter)
+        simulated_pose.latitude_deg = lat_long[0]
+        simulated_pose.longitude_deg = lat_long[1]
 
         simulator_pub.publish(simulated_pose)
 
