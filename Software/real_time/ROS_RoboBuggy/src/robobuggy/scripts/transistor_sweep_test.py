6c350f871590d38c032a68e6503b90d6f536dc04
Vivaan Bahl <vivaanbahl@gmail.com>
2018-11-09 18:35:53 -0500

    modified sweep test

--- Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/transistor_sweep_test.py
+++ Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/transistor_sweep_test.py
@@ -2,6 +2,7 @@
 
 import rospy
 from robobuggy.msg import Command
+import math
 
 def sweep_steering():
     pub = rospy.Publisher('Command', Command, queue_size=1000)
@@ -12,7 +13,7 @@ def sweep_steering():
     steering_change = 100
     command_msg = Command()
     while not rospy.is_shutdown():
-        command_msg.steer_cmd = steering_pos
+        command_msg.steer_cmd_rad = math.radians(steering_pos/100);
         command_msg.brake_cmd = False
         pub.publish(command_msg)
         steering_pos += steering_change
