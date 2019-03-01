7ca712c1bd53e05442b7da02842915607325b28e
Cn-2000 <ningc@andrew.cmu.edu>
2019-03-01 18:49:43 -0500

    bug fixes

--- Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/visualization.py
+++ Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/visualization.py
@@ -5,15 +5,16 @@ from robobuggy.msg import Diagnostics
 from robobuggy.msg import Feedback
 from robobuggy.msg import Pose
 from Tkinter import *
+import math
 
 # Get brake state, commanded steering, feedback steering, current orientation,
 # battery level and visualize them on a user interfate
 
 def commandUpdate(data):
-    if data.steer_cmd_rad>0:
-        steer_cmd_rad = "%10.4f rad left" % data.steer_cmd_rad
-    elif data.steer_cmd_rad=0:
+    if abs(data.steer_cmd_rad) < 0.0001:
         steer_cmd_rad = "Straight"
+    elif data.steer_cmd_rad>0:
+        steer_cmd_rad = "%10.4f rad left" % data.steer_cmd_rad
     else:
         steer_cmd_rad = "%10.4f rad right" % (data.steer_cmd_rad * -1)
     canvas.itemconfigure(t1, text=steer_cmd_rad)
@@ -30,7 +31,23 @@ def feedbackUpdate(data):
         canvas.itemconfigure(t3, text="Suspended")
 
 def poseUpdate(data):
-    heading_rad = "%10.4f" % data.heading_rad
+    heading_rad = "%10.4f heading " % data.heading_rad
+    if abs(data.heading_rad) < 0.0001:
+        heading_rad += "E"
+    elif abs(data.heading_rad - math.pi / 2) < 0.0001:
+        heading_rad += "N"
+    elif abs(data.heading_rad + math.pi / 2) < 0.0001:
+        heading_rad += "S"
+    elif abs(abs(data.heading_rad) - math.pi) < 0.0001:
+        heading_rad += "W"
+    elif 0 < data.heading_rad and data.heading_rad < math.pi / 2:
+        heading_rad += "NE"
+    elif math.pi / 2 < data.heading_rad and data.heading_rad < math.pi:
+        heading_rad += "NW"
+    elif -math.pi / 2 < data.heading_rad and data.heading_rad < 0:
+        heading_rad += "SE"
+    else:
+        heading_rad += "SW"
     canvas.itemconfigure(t4, text=heading_rad)
 
 def listener():
@@ -47,7 +64,7 @@ if __name__ == '__main__':
     # Initialize GUI
     root = Tk()
     root.title("Robobuggy Status")
-    root.resizable(width=False, height=False)
+    root.resizable(width=True, height=True)
     canvas = Canvas(root, width=500, height=300)
     canvas.configure(bd=0, highlightthickness=0)
     canvas.pack()
