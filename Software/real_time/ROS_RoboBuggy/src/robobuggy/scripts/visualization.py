186fef3de807e955dea2755cb672e2905bd03757
Cn-2000 <ningc@andrew.cmu.edu>
2019-03-02 14:58:01 -0500

    changed radians to degrees; GUI modifications

--- Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/visualization.py
+++ Software/real_time/ROS_RoboBuggy/src/robobuggy/scripts/visualization.py
@@ -11,44 +11,46 @@ import math
 # battery level and visualize them on a user interfate
 
 def commandUpdate(data):
-    if abs(data.steer_cmd_rad) < 0.0001:
-        steer_cmd_rad = "Straight"
-    elif data.steer_cmd_rad>0:
-        steer_cmd_rad = "%10.4f rad left" % data.steer_cmd_rad
+    steer_cmd_deg = data.steer_cmd_rad * 180 / math.pi
+    if abs(steer_cmd_deg) < 0.1:
+        steer_cmd = "Straight"
+    elif steer_cmd_deg > 0:
+        steer_cmd = "%10.1f deg left" % steer_cmd_deg
     else:
-        steer_cmd_rad = "%10.4f rad right" % (data.steer_cmd_rad * -1)
-    canvas.itemconfigure(t1, text=steer_cmd_rad)
+        steer_cmd = "%10.1f deg right" % (steer_cmd_deg * -1)
+    t0.set(steer_cmd + "   ")
 
 def diagnosticsUpdate(data):
     voltage = data.battery_level / 1000.00
     battery_level = "%10.3f V" % voltage
-    canvas.itemconfigure(t2, text=battery_level)
+    t1.set(battery_level + "   ")
 
 def feedbackUpdate(data):
     if data.brake_state==True:
-        canvas.itemconfigure(t3, text="Deployed")
+        t2.set("Deployed   ")
     else:
-        canvas.itemconfigure(t3, text="Suspended")
+        t2.set("Suspended   ")
 
 def poseUpdate(data):
-    heading_rad = "%10.4f heading " % data.heading_rad
-    if abs(data.heading_rad) < 0.0001:
-        heading_rad += "E"
-    elif abs(data.heading_rad - math.pi / 2) < 0.0001:
-        heading_rad += "N"
-    elif abs(data.heading_rad + math.pi / 2) < 0.0001:
-        heading_rad += "S"
-    elif abs(abs(data.heading_rad) - math.pi) < 0.0001:
-        heading_rad += "W"
-    elif 0 < data.heading_rad and data.heading_rad < math.pi / 2:
-        heading_rad += "NE"
-    elif math.pi / 2 < data.heading_rad and data.heading_rad < math.pi:
-        heading_rad += "NW"
-    elif -math.pi / 2 < data.heading_rad and data.heading_rad < 0:
-        heading_rad += "SE"
+    heading_deg = data.heading_rad * 180 / math.pi
+    heading = "%10.1f deg heading " % heading_deg
+    if abs(heading_deg) < 0.1:
+        heading += "E"
+    elif abs(heading_deg - 90) < 0.1:
+        heading += "N"
+    elif abs(heading_deg + 90) < 0.1:
+        heading += "S"
+    elif abs(abs(heading_deg) - 180) < 0.1:
+        heading += "W"
+    elif 0 < heading_deg and heading_deg < 90:
+        heading += "NE"
+    elif 90 < heading_deg and heading_deg < 180:
+        heading += "NW"
+    elif -90 < heading_deg and heading_deg < 0:
+        heading += "SE"
     else:
-        heading_rad += "SW"
-    canvas.itemconfigure(t4, text=heading_rad)
+        heading += "SW"
+    t3.set(heading + "   ")
 
 def listener():
 
@@ -61,21 +63,35 @@ def listener():
 
 if __name__ == '__main__':
 
-    # Initialize GUI
     root = Tk()
     root.title("Robobuggy Status")
     root.resizable(width=True, height=True)
-    canvas = Canvas(root, width=500, height=300)
-    canvas.configure(bd=0, highlightthickness=0)
-    canvas.pack()
-    canvas.create_text(20, 45, text="Commanded Steering:", anchor="w", fill="black", font="Times 20 bold")
-    canvas.create_text(20, 115, text="Battery Level:", anchor="w", fill="black", font="Times 20 bold")
-    canvas.create_text(20, 185, text="Brake State:", anchor="w", fill="black", font="Times 20 bold")
-    canvas.create_text(20, 255, text="Current Orientation:", anchor="w", fill="black", font="Times 20 bold")
-    t1 = canvas.create_text(480, 45, text="Fetching data...", anchor="e", fill="black", font="Times 16")
-    t2 = canvas.create_text(480, 115, text="Fetching data...", anchor="e", fill="black", font="Times 16")
-    t3 = canvas.create_text(480, 185, text="Fetching data...", anchor="e", fill="black", font="Times 16")
-    t4 = canvas.create_text(480, 255, text="Fetching data...", anchor="e", fill="black", font="Times 16")
+
+    l0 = Label(root, text = " Commanded Steering: ", font = ("Times", 20, "bold")).grid(row = 0, column = 0, sticky = W)
+    l1 = Label(root, text = " Battery Level: ", font = ("Times", 20, "bold")).grid(row = 1, column = 0, sticky = W)
+    l2 = Label(root, text = " Brake State: ", font = ("Times", 20, "bold")).grid(row = 2, column = 0, sticky = W)
+    l3 = Label(root, text = " Current Orientation: ", font = ("Times", 20, "bold")).grid(row =3, column = 0, sticky = W)
+
+    t0 = StringVar()
+    t1 = StringVar()
+    t2 = StringVar()
+    t3 = StringVar()
+    t0.set("Fetching data...   ")
+    t1.set("Fetching data...   ")
+    t2.set("Fetching data...   ")
+    t3.set("Fetching data...   ")
+
+    w0 = Label(root, textvariable = t0, font = ("Times", 16)).grid(row = 0, column = 1, sticky = E)
+    w1 = Label(root, textvariable = t1, font = ("Times", 16)).grid(row = 1, column = 1, sticky = E)
+    w2 = Label(root, textvariable = t2, font = ("Times", 16)).grid(row = 2, column = 1, sticky = E)
+    w3 = Label(root, textvariable = t3, font = ("Times", 16)).grid(row = 3, column = 1, sticky = E)
+
+    root.grid_columnconfigure(0, weight = 1)
+    root.grid_columnconfigure(1, weight = 1)
+    root.grid_rowconfigure(0, weight = 1)
+    root.grid_rowconfigure(1, weight = 1)
+    root.grid_rowconfigure(2, weight = 1)
+    root.grid_rowconfigure(3, weight = 1)
 
     listener()
     root.mainloop()
